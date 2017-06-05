cfb62fa851849aa961e4b8e3c3f63e9cb02e6874
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-06-05 02:15:19 -0400

    db migrations for user table

---
+++ database/migrations/2017_06_04_231655_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uid', 127)->unique();
+            $table->unsignedInteger('gtid')->unique();
+            $table->string('slack_id', 21)->nullable();
+            $table->string('first_name', 127);
+            $table->string('middle_name', 127)->nullable();
+            $table->string('last_name', 127);
+            $table->string('preferred_name', 127)->nullable();
+            $table->string('name')->virtualAs("CONCAT(first_name,' ',last_name)")->nullable();
+            //$table->string('full_name')->virtualAs("CONCAT(first_name,' ',middle_name,' ',last_name)");
+            $table->string('phone', 15)->nullable();
+            $table->string('emergency_contact_name')->nullable();
+            $table->string('emergency_contact_phone', 15)->nullable();
+            $table->char('join_date', 6)->nullable();
+            $table->char('graduation_date', 6)->nullable();
+            $table->enum('shirt_size', ['s', 'm', 'l', 'xl', 'xxl', 'xxxl'])->nullable();
+            $table->enum('polo_size', ['s', 'm', 'l', 'xl', 'xxl', 'xxxl'])->nullable();
+            $table->string('gt_email');
+            $table->string('personal_email')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
