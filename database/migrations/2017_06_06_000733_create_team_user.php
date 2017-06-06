ec8fee906053c5d1107ccdad86aa465cb7c5b7bc
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-06-05 20:13:06 -0400

    created team_user table migration
    fix style error

---
+++ database/migrations/2017_06_06_000733_create_team_user.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('team_id');
+            $table->unsignedInteger('user_id');
+
+            $table->foreign('team_id')->references('id')->on('teams');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('team_user', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['team_id']);
+        });
+
+        Schema::dropIfExists('team_user');
+    }
+}
