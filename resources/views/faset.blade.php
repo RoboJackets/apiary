fa0d7c8b1fc21b16e02c3db8fe232d2e4c93ff16
Kristaps Berzinch <kristapsberzinch@gmail.com>
2017-06-17 10:11:33 -0500

    Form submits via AJAX

--- resources/views/faset.blade.php
+++ resources/views/faset.blade.php
@@ -11,64 +11,101 @@
   <body>
     <div class="ph6-l ph5-m pa4">
       <div class="normal black-60 ph4-ns pt4-ns red">All fields are required.</div>
-      <form class="pa4-ns near-black" method="POST" action="/api/v1/faset">
+      <form id="form" class="pa4-ns near-black" v-on:submit.prevent="submit">
         <label for="name" class="f6 b db mb2">Name</label>
-        <input id="name" class="input-reset ba b--black-20 pa2 mb2 db w-100 mb4" type="text" aria-describedby="name-desc" required>
+        <input name="faset-name" id="name" class="input-reset ba b--black-20 pa2 mb2 db w-100 mb4" type="text" aria-describedby="name-desc" required>
         <label for="email" class="f6 b db mb2">Email</label>
-        <input id="email" class="input-reset ba b--black-20 pa2 mb2 db w-100 mb4" type="email" aria-describedby="email" required>
+        <input name="faset-email" id="email" class="input-reset ba b--black-20 pa2 mb2 db w-100 mb4" type="email" aria-describedby="email" required>
         <legend class="fw7 mb2">How did you hear about us?</legend>
         <div class="flex items-center mb2">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam">
-          <label for="spacejam" class="lh-copy">FASET</label>
+          <input id="faset" class="mr2" type="checkbox" name="heardfrom" value="faset">
+          <label for="faset" class="lh-copy">FASET</label>
         </div>
         <div class="flex items-center mb2">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam">
-          <label for="spacejam" class="lh-copy">Campus tour</label>
+          <input id="tour" class="mr2" type="checkbox" name="heardfrom" value="tour">
+          <label for="tour" class="lh-copy">Campus tour</label>
         </div>
         <div class="flex items-center mb2">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam">
-          <label for="spacejam" class="lh-copy">Current RoboJackets member</label>
+          <input id="member" class="mr2" type="checkbox" name="heardfrom" value="member">
+          <label for="member" class="lh-copy">Current RoboJackets member</label>
         </div>
         <div class="flex items-center mb2">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam">
-          <label for="spacejam" class="lh-copy">Social media</label>
+          <input id="social" class="mr2" type="checkbox" name="heardfrom" value="social">
+          <label for="social" class="lh-copy">Social media</label>
         </div>
         <div class="flex items-center mb2">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam">
-          <label for="spacejam" class="lh-copy">RoboJackets.org</label>
+          <input id="web" class="mr2" type="checkbox" name="heardfrom" value="web">
+          <label for="web" class="lh-copy">RoboJackets.org</label>
         </div>
         <div class="flex items-center mb2">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam">
-          <label for="spacejam" class="lh-copy">FRC event</label>
+          <input id="frc" class="mr2" type="checkbox" name="heardfrom" value="frc">
+          <label for="frc" class="lh-copy">FRC event</label>
         </div>
         <div class="flex items-center mb2">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam">
-          <label for="spacejam" class="lh-copy">FTC event</label>
+          <input id="ftc" class="mr2" type="checkbox" name="heardfrom" value="ftc">
+          <label for="ftc" class="lh-copy">FTC event</label>
         </div>
         <div class="flex items-center mb2">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam">
-          <label for="spacejam" class="lh-copy">VEX event</label>
+          <input id="vex" class="mr2" type="checkbox" name="heardfrom" value="vex">
+          <label for="vex" class="lh-copy">VEX event</label>
         </div>
-        <div id="otherbox" class="flex items-center mb4">
-          <input class="mr2" type="checkbox" id="spacejam" value="spacejam" v-model="isChecked">
-          <label for="spacejam" class="lh-copy">Other: &nbsp</label>
-          <input class="mr2" type="text" id="spacejam" value="" v-bind:required="isRequired">
+        <div id="otherdiv" class="flex items-center mb4">
+          <input id="other" class="mr2" type="checkbox" name="heardfrom" value="other" v-model="isChecked">
+          <label for="other" class="lh-copy">Other: &nbsp</label>
+          <input name="other" class="mr2" type="text" value="" v-bind:required="isRequired">
         </div>
         <input class="b ph3 pv2 input-reset ba b--black bg-transparent dim pointer f6 dib" type="submit" value="Submit">
       </form>
     </div>
     <script src="https://unpkg.com/vue"></script>
     <script type="text/javascript">
-      var vm = new Vue({
-        el: '#otherbox',
+      var ajax = new Vue({
+        el: 'form',
         data: {
           isChecked: false
         },
         computed: {
           isRequired: function () {
-            return this.isChecked
+            return this.isChecked;
           }
+        },
+        methods: {
+
+              submit: function() {
+          var handleError = (function(err) {
+              alert(err);
+          }).bind(this);
+
+          var handleFinish = (function(data) {
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+              alert("Your form response has been saved. Thanks for stopping by!");
+              document.getElementById("form").reset();
+            }
+          }).bind(this);
+
+          var xhr = new XMLHttpRequest();
+          xhr.open("POST", "api/v1/faset", true);
+          xhr.responseType = 'json';
+
+          xhr.addEventListener('readystatechange', handleFinish);
+          xhr.addEventListener('error', handleError);
+          xhr.addEventListener('abort', handleError);
+          xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+          var formData = new FormData(event.target);
+          result = {};
+          result['faset-email'] = formData.get('faset-email');
+          result['faset-name'] = formData.get('faset-name');
+          result['faset-questions'] = [];
+          values = formData.getAll('heardfrom');
+          if (values.includes('other')) {
+            values.splice(values.indexOf('other'), 1);
+            values.push(formData.get('other'));
+          }
+          result['faset-questions'][0] = {"1" : values}
+          xhr.send(JSON.stringify(result));
         }
+      }
       })
     </script>
   </body>
