341faaeb98f0f50f60c6d3e58cc3968d494954a0
Kristaps Berzinch <kristapsberzinch@gmail.com>
2017-06-18 20:05:10 -0500

    Added basic CAS authentication

---
+++ app/Http/Middleware/CASAuthenticate.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kberz
+ * Date: 6/18/2017
+ * Time: 7:34 PM
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class CASAuthenticate
+{
+    protected $auth;
+    protected $cas;
+
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+        $this->cas = app('cas');
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if( $this->cas->isAuthenticated() )
+        {
+            // Store the user credentials in a Laravel managed session
+            session()->put('cas_user', $this->cas->user());
+        } else {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response('Unauthorized', 401);
+            }
+            $this->cas->authenticate();
+        }
+        return $next($request);
+    }
+}
--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Subfission\Cas\CasServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Cas' => Subfission\Cas\Facades\Cas::class,
 
     ],
 
