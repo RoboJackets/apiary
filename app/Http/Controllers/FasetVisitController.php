d31bdf7183930a888d04855162f9babb8b26efd7
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-07-06 12:14:10 -0400

    Make Pretty

--- app/Http/Controllers/FasetVisitController.php
+++ app/Http/Controllers/FasetVisitController.php
@@ -46,7 +46,7 @@ class FasetVisitController extends Controller
     
     public function list(Request $request)
     {
-        $visits = FasetVisit::all()->select('id','visit_token','created_at');
+        $visits = FasetVisit::all()->select('id', 'visit_token', 'created_at');
 
         return response()->json($visits);
     }
