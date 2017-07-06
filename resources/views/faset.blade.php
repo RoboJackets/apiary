b5d82d31dcfc417d01132a9fe4fd6f1d3771610a
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-07-05 22:18:42 -0400

    Added favicons as a Blade sub-view

--- resources/views/faset.blade.php
+++ resources/views/faset.blade.php
@@ -4,6 +4,8 @@
     <title>FASET Form | MyRoboJackets</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+
+    @include('favicon')
   </head>
   <body id="faset">
     <div class="container">
