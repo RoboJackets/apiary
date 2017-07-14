dd4d6f3c9b2ccea40752973ec9529a5f0777acab
RossIV <ross@rosslindsay.com>
2017-07-13 21:28:19 -0400

    #78 Fixed the migration, it actually works now

--- database/migrations/2017_07_13_035859_update_user_name_concat.php
+++ database/migrations/2017_07_13_035859_update_user_name_concat.php
@@ -13,11 +13,9 @@ class UpdateUserNameConcat extends Migration
      */
     public function up()
     {
-        Schema::getConnection()->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
-
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('name')->virtualAs("concat_ws(' ',`first_name`,`last_name`)")->change();
-            $table->string('full_name')->virtualAs("concat_ws(' ',`first_name`,`middle_name`,`last_name`)")->change();
+        DB::transaction(function() {
+           DB::statement("ALTER TABLE `users` CHANGE COLUMN `name` `name` VARCHAR(255) GENERATED ALWAYS AS (concat_ws(' ',`first_name`,`last_name`)) VIRTUAL;");
+           DB::statement("ALTER TABLE `users` CHANGE COLUMN `full_name` `full_name` VARCHAR(255) GENERATED ALWAYS AS (concat_ws(' ',`first_name`,`middle_name`,`last_name`)) VIRTUAL");
         });
     }
 
@@ -28,11 +26,9 @@ class UpdateUserNameConcat extends Migration
      */
     public function down()
     {
-        Schema::getConnection()->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
-
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('name')->virtualAs("CONCAT(first_name,' ',last_name)")->change();
-            $table->string('full_name')->virtualAs("CONCAT(first_name,' ',middle_name,' ',last_name)")->change();
+        DB::transaction(function() {
+            DB::statement("ALTER TABLE `users` CHANGE COLUMN `name` `name` VARCHAR(255) GENERATED ALWAYS AS (CONCAT(first_name,' ',last_name)) VIRTUAL;");
+            DB::statement("ALTER TABLE `users` CHANGE COLUMN `full_name` `full_name` VARCHAR(255) GENERATED ALWAYS AS (CONCAT(first_name,' ',middle_name,' ',last_name)) VIRTUAL");
         });
     }
 }
