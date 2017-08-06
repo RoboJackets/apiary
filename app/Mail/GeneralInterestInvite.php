6fb3fb5cccbb6d93cada400c9b0389dd109bdb68
Kristaps Berzinch <kristapsberzinch@gmail.com>
2017-08-06 15:21:09 -0500

    Added basic General Interest invite design

---
+++ app/Mail/GeneralInterestInvite.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class GeneralInterestInvite extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $this->from('noreply@my.robojackets.org', 'RoboJackets')
+             ->subject('RoboJackets General Interest Meeting - RSVP Requested')
+             ->markdown('mail.generalinterest.invite');
+
+        $this->withSwiftMessage(function ($message) {
+        $message->getHeaders()
+                ->addTextHeader('Reply-To', 'RoboJackets Support <support@robojackets.org>');
+        });
+    }
+}
---
+++ resources/views/mail/generalinterest/invite.blade.php
@@ -0,0 +1,18 @@
+@component('mail::message')
+# Calling all future RoboJackets!
+
+RoboJackets builds robots and is active in the Atlanta robotics community to promote robotics, educate its members, and advance robotics. Competing in five competitions annually, RoboJackets is the largest robotics student organization at Georgia Tech.
+
+We are holding our General Interest Meetings on **Tuesday, September 6 in Howey L3** and **Wednesday, September 7 in Howey L4**. Any student interested in joining RoboJackets should attend one of the two meetings, which will feature the same content. This is the meeting where we’ll tell interested students all about our different teams, the projects we’ll be working on this year, and when we’ll be meeting this semester. All skill levels are welcome. If you can’t make it, don’t worry! We’ll be sending out all of the information to our general mailing list, which you can join [here](https://lists.gatech.edu/sympa/subscribe/robojackets-general).
+
+Pizza and drinks will be provided. Click a day below to RSVP.
+
+@component('mail::button', ['url' => 'https://not-a-real-domain.robojackets.org/clicked-button-one'])
+RSVP for Tuesday, September 6
+@endcomponent
+
+@component('mail::button', ['url' => 'https://not-a-real-domain.robojackets.org/clicked-button-two'])
+RSVP for Wednesday, September 7
+@endcomponent
+
+@endcomponent
