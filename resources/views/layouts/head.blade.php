376d024b98504572c13a5325e391dace7648a94f
Kristaps Berzinch <kristapsberzinch@gmail.com>
2017-08-07 18:26:58 -0500

    Adding Google Analytics

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,3 +6,12 @@
 
 
 @include('favicon')
+
+<!-- Google Analytics -->
+<script>
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('send', 'pageview');
+</script>
+<script async src='https://www.google-analytics.com/analytics.js'></script>
+<!-- End Google Analytics -->
