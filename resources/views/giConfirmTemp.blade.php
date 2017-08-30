6f319adc7ed33421bb589c9f461eba40267ea152
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-08-30 00:04:55 -0400

    most functionality for oneClickRSVP functionality

---
+++ resources/views/giConfirmTemp.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>General Interest | MyRoboJackets</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+
+    @include('favicon')
+  </head>
+  <body id="faset">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <img class="mt-2 px-5 img-fluid" src="{{ asset('img/faset_form_header.svg') }}">
+          <hr class="my-4">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+
