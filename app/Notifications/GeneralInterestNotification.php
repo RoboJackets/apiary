940d30aff6d7756ea6eba9fbf7b5438953ff36f7
RossIV <ross@rosslindsay.com>
2017-08-30 02:00:01 -0400

    Create Notification for sending GI mailable

---
+++ app/Notifications/GeneralInterestNotification.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Mail\GeneralInterestInvite as Mailable;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class GeneralInterestNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param \App\FasetVisit $visit
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return Mailable
+     */
+    public function toMail($notifiable)
+    {
+        return (new Mailable($notifiable->visit_token))->to($notifiable->faset_email);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
