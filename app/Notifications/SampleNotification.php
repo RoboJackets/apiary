fc1a6a432799c8ad1ee5f368a379785b3c8c1d38
RossIV <ross@rosslindsay.com>
2017-08-30 00:55:22 -0400

    Implement shouldqueue

--- app/Notifications/SampleNotification.php
+++ app/Notifications/SampleNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class SampleNotification extends Notification
+class SampleNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
