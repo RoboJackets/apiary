3baff3e47071814cf3308abda93bdfc907b22ca9
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-08-30 00:36:37 -0400

    style

--- database/migrations/2017_08_29_163537_create_events_table.php
+++ database/migrations/2017_08_29_163537_create_events_table.php
@@ -13,7 +13,6 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
