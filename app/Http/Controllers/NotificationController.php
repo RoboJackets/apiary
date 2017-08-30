cf6e2b604236ac9c06a429efc2a38dd6b62743c9
RossIV <ross@rosslindsay.com>
2017-08-30 02:38:41 -0400

    Add chunking to send 30 messages per hour to overcome GT rate limiting

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use App\FasetVisit;
 use App\Notifications\GeneralInterestNotification;
+use Carbon\Carbon;
+use Notification;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
     public function sendNotification()
     {
-        $all_faset_vists = FasetVisit::all();
-        Notification::send($all_faset_vists, new GeneralInterestNotification());
+        $hours = 0;
+        FasetVisit::chunk(30, function ($chunk) use (&$hours) {
+            $when = Carbon::now()->addHours($hours);
+            Notification::send($chunk, (new GeneralInterestNotification())->delay($when));
+            $hours++;
+        });
         return response()->json(['status' => 'success']);
     }
 }
