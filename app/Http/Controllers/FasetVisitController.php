90d8510d4a2ebc13ed9cde3dec35c7f9df0af012
RossIV <ross@rosslindsay.com>
2017-08-31 20:04:46 -0400

    Someone else's typo

--- app/Http/Controllers/FasetVisitController.php
+++ app/Http/Controllers/FasetVisitController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\FasetVisit;
-use App\fasetResponse;
+use App\FasetResponse;
 
 class FasetVisitController extends Controller
 {
