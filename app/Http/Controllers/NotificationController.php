a7e2d6904ee4548dbdf4104252b58aeaf87601c2
RossIV <ross@rosslindsay.com>
2017-09-01 20:02:28 -0400

    Add function and route to allow for sending emails manually by POST-ing a list

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -20,4 +20,21 @@ class NotificationController extends Controller
         });
         return response()->json(['status' => 'success']);
     }
+    
+    public function sendNotificationManual(Request $request)
+    {
+        if (!$request->has('emails')) {
+            return response()->json(['status' => 'error', 'error' => "Missing parameter 'emails'"], 400);
+        }
+        
+        $hours = 0;
+        $emails = $request->input('emails');
+        $chunks = array_chunk($emails, 30);
+        foreach ($chunks as $chunk) {
+            $when = Carbon::now()->addHours($hours);
+            Notification::send($chunk, (new GeneralInterestNotification())->delay($when));
+            $hours++;
+        }
+        return response()->json(['status' => 'success', 'count' => count($emails)]);
+    }
 }
