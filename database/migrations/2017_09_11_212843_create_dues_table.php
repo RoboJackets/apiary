3e1fa455ebe00e7695daddf64cde8477e6971f09
RossIV <ross@rosslindsay.com>
2017-09-11 17:54:52 -0400

    Add first draft of migrations for dues and payments tables

---
+++ database/migrations/2017_09_11_212843_create_dues_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('eligible_for_shirt')->default(false);
+            $table->boolean('eligible_for_polo')->default(false);
+            $table->boolean('received_shirt')->default(false);
+            $table->boolean('received_polo')->default(false);
+            $table->timestamp('effective_start');
+            $table->timestamp('effective_end');
+            $table->unsignedInteger('payment_id');
+
+            $table->foreign('payment_id')->references('id')->on('payments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dues');
+    }
+}
