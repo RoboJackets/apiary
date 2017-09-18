90b9ee65958ec8a3213944ec7102f1b0fa52781b
RossIV <ross@rosslindsay.com>
2017-09-17 22:26:13 -0400

    Add controllers for DuesPackage, DuesTransaction, and Payment models

---
+++ app/Http/Controllers/DuesPackageController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\DuesPackage;
+
+class DuesPackageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $packages = DuesPackage::all();
+        return response()->json(['status' => 'success', 'packages' => $packages]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'eligible_for_shirt' => 'boolean',
+            'eligible_for_polo' => 'boolean',
+            'effective_start' => 'required|date',
+            'effective_end' => 'required|date',
+            'cost' => 'required|numeric'
+        ]);
+
+        try {
+            $package = DuesPackage::create($request->all());
+        } catch (QueryException $e) {
+            $errorMessage = $e->errorInfo[2];
+            return response()->json(['status' => 'error', 'message' => $errorMessage], 500);
+        }
+
+        if (is_numeric($package->id)) {
+            $dbPackage = DuesPackage::findOrFail($package->id);
+            return response()->json(['status' => 'success', 'package' => $dbPackage], 201);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Unknown error.'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $package = DuesPackage::find($id);
+        if ($package) {
+            return response()->json(['status' => 'success', 'package' => $package]);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Payment not found.'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'id' => 'required|exists:dues_packages',
+            'name' => 'string',
+            'eligible_for_shirt' => 'boolean',
+            'eligible_for_polo' => 'boolean',
+            'effective_start' => 'date',
+            'effective_end' => 'date',
+            'cost' => 'numeric'
+        ]);
+
+        $package = DuesPackage::find($id);
+        $package->update($request->all());
+
+        $package = DuesPackage::find($package->id);
+        if ($package) {
+            return response()->json(['status' => 'success', 'package' => $package]);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Unknown error.'], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $package = DuesPackage::find($id);
+        $deleted = $package->delete();
+        if ($deleted) {
+            return response()->json(['status' => 'success', 'message' => 'DuesPackage deleted.']);
+        } else {
+            return response()->json(['status' => 'error',
+                'message' => 'DuesPackage does not exist or was previously deleted.'], 422);
+        }
+    }
+}
---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Payment;
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $payments = Payment::all();
+        return response()->json(['status' => 'success', 'payments' => $payments]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'amount' => 'required|numeric',
+            'payment_method' => 'required|string',
+            'recorded_by' => 'required|numeric|exists:users,id'
+        ]);
+
+        try {
+            $payment = Payment::create($request->all());
+        } catch (QueryException $e) {
+            $errorMessage = $e->errorInfo[2];
+            return response()->json(['status' => 'error', 'message' => $errorMessage], 500);
+        }
+
+        if (is_numeric($payment->id)) {
+            $dbPayment = Payment::findOrFail($payment->id);
+            return response()->json(['status' => 'success', 'payment' => $dbPayment], 201);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Unknown error.'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $payment = Payment::find($id);
+        if ($payment) {
+            return response()->json(['status' => 'success', 'payment' => $payment]);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Payment not found.'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'id' => 'required|exists:payments',
+            'amount' => 'required|numeric',
+            'payment_method' => 'required|string',
+            'recorded_by' => 'required|numeric|exists:users,id'
+        ]);
+
+        $payment = Payment::find($id);
+        $payment->update($request->all());
+
+        $payment = Payment::find($payment->id);
+        if ($payment) {
+            return response()->json(['status' => 'success', 'payment' => $payment]);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Unknown error.'], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $payment = Payment::find($id);
+        $deleted = $payment->delete();
+        if ($deleted) {
+            return response()->json(['status' => 'success', 'message' => 'Payment deleted.']);
+        } else {
+            return response()->json(['status' => 'error',
+                'message' => 'Payment does not exist or was previously deleted.'], 422);
+        }
+    }
+}
