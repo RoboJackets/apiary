1d65c42029bd137ef2296b117095e681d624b2b8
RossIV <ross@rosslindsay.com>
2017-09-17 22:52:36 -0400

    Add 'guarded' to allow for mass assignment via Eloquent; Add User and DuesTransaction Eloquent relationships

--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * Get the User associated with the Payment model.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'recorded_by');
+    }
+
+    /**
+     * Get the DuesTransaction
+     */
+    public function duesTransaction()
+    {
+        return $this->hasOne('App\DuesTransaction');
+    }
+
 }
