8b700bf2a0390c5f8aa7cbd25d8cb0457782b51d
RossIV <ross@rosslindsay.com>
2017-09-21 16:09:43 -0400

    Add is_active virtual attribute, available_for_purchase local scope, active local scope

--- app/DuesPackage.php
+++ app/DuesPackage.php
@@ -14,11 +14,29 @@ class DuesPackage extends Model
     protected $guarded = ['id'];
 
     /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['is_active'];
+
+    /**
      * Get the DuesTransaction associated with the DuesPackage model.
      */
     public function transactions()
     {
         return $this->hasMany('App\DuesTransaction');
+    }
+
+    /**
+     * Scope a query to only include DuesPackages available for purchase.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAvailableForPurchase($query)
+    {
+        return $query->where('available_for_purchase', 1);
     }
 
     /**
@@ -29,6 +47,18 @@ class DuesPackage extends Model
      */
     public function scopeActive($query)
     {
-        return $query->where('active', 1);
+        return $query->whereDate('effective_start', '<=', date('Y-m-d'))
+            ->whereDate('effective_end', '>=', date('Y-m-d'));
+    }
+
+    /**
+     * Get the is_active flag for the DuesPackage.
+     *
+     * @return bool
+     */
+    public function getIsActiveAttribute()
+    {
+        return ($this->attributes['effective_start'] <= date('Y-m-d') &&
+                $this->attributes['effective_end'] >= date('Y-m-d'));
     }
 }
