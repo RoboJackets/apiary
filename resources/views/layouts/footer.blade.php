96063ba3ab743393a7f4f44b1c63426a07c83acb
Ryan Strat <ryanstrat@users.noreply.github.com>
2017-10-10 22:38:34 -0400

    Open Make a Wish Link in a new tab

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
         <p class="text-muted text-right">Made with ♥ by RoboJackets</p>
       </div>
       <div>
-        <a class="text-muted text-left" href="https://docs.google.com/forms/d/e/1FAIpQLSelERsYq3gLmHbWvVCWha5iCU8z3r9VYC0hCN4ArLpMAiysaQ/viewform?entry.1338203640={{ $request->fullUrl()}}">Make a Wish</a>
+        <a class="text-muted text-left" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSelERsYq3gLmHbWvVCWha5iCU8z3r9VYC0hCN4ArLpMAiysaQ/viewform?entry.1338203640={{ $request->fullUrl()}}">Make a Wish</a>
       </div>
     </div>
   </div>
