88fe84abf021e5205c9f5040ea874b3edff2ed60
RossIV <ross@rosslindsay.com>
2017-11-04 14:40:36 -0400

    Add permission middleware; Tweak validation rules; Finished show, update, destroy functions

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -8,6 +8,15 @@ use Illuminate\Database\QueryException;
 
 class AttendanceController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('permission:read-attendance', ['only' => ['index']]);
+        $this->middleware('permission:create-attendance', ['only' => ['store']]);
+        $this->middleware('permission:read-attendance|read-attendance-own', ['only' => ['show']]);
+        $this->middleware('permission:update-attendance', ['only' => ['update']]);
+        $this->middleware('permission:delete-attendance', ['only' => ['destroy']]);
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -28,11 +37,11 @@ class AttendanceController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'attendable_type' => 'required',
+            'attendable_type' => 'required|string',
             'attendable_id' => 'required|numeric',
             'gtid' => 'numeric|max:9|exists:users',
             'source' => 'required|string',
-            'recorded_by' => 'required|numeric|exists:users'
+            'recorded_by' => 'required|numeric|exists:users,id'
         ]);
 
         try {
@@ -53,34 +62,69 @@ class AttendanceController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Attendance  $attendance
+     * @param  \Illuminate\Http\Request  $request
+     * @param  $id integer Resource ID
      * @return \Illuminate\Http\Response
      */
-    public function show(Attendance $attendance)
+    public function show(Request $request, $id)
     {
-        //
+        $user = auth()->user();
+        $att = Attendance::find($id);
+        if ($att && ($att->gtid == $user->gtid || $user->can('read-attendance'))) {
+            return response()->json(['status' => 'success', 'attendance' => $att]);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Attendance not found.'], 404);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Attendance  $attendance
+     * @param  $id integer Resource ID
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Attendance $attendance)
+    public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'attendable_type' => 'string',
+            'attendable_id' => 'numeric',
+            'gtid' => 'numeric|exists:users',
+            'source' => 'string',
+            'recorded_by' => 'numeric|exists:users'
+        ]);
+
+        $att = Attendance::find($id);
+        if ($att) {
+            $att->update($request->all());
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Attendance not found.'], 404);
+        }
+
+        $att = Attendance::find($att->id);
+        if ($att) {
+            return response()->json(['status' => 'success', 'attendance' => $att]);
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Unknown error.'], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Attendance  $attendance
+     * @param  \Illuminate\Http\Request  $request
+     * @param  $id integer Resource ID
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Attendance $attendance)
+    public function destroy(Request $request, $id)
     {
-        //
+        $att = Attendance::find($id);
+        $deleted = $att->delete();
+        if ($deleted) {
+            return response()->json(['status' => 'success', 'message' => 'Attendance deleted.']);
+        } else {
+            return response()->json(['status' => 'error',
+                'message' => 'Attendance does not exist or was previously deleted.'], 422);
+        }
     }
 }
