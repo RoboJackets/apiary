b0cc78869cd123fbbdbc08ea0b4cab69b9907a18
Ross Lindsay <ross@rosslindsay.com>
2017-11-19 19:50:12 -0500

    Add storeUser + createSquareCheckout functions to allow for user-facing payment without admin interaction using Square E-Commerce

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Payment;
+use App\Event;
+use App\DuesTransaction;
+use SquareConnect\Api\CheckoutApi;
+use SquareConnect\Configuration;
+use SquareConnect\Model\CreateCheckoutRequest;
+use SquareConnect\Model\CreateOrderRequest;
 use App\Notifications\Payment\ConfirmationNotification as Confirm;
 
 class PaymentController extends Controller
@@ -60,12 +66,82 @@ class PaymentController extends Controller
 
         if (is_numeric($payment->id)) {
             $dbPayment = Payment::findOrFail($payment->id);
-
             $dbPayment->payable->user->notify(new Confirm($dbPayment));
 
             return response()->json(['status' => 'success', 'payment' => $dbPayment], 201);
         } else {
             return response()->json(['status' => 'error', 'message' => 'Unknown error.'], 500);
+        }
+    }
+
+    /**
+     * Handles payment request from user-facing UI
+     *
+     * @param Request $request
+     * @return mixed
+     */
+    public function storeUser(Request $request)
+    {
+        $user = auth()->user();
+        $payable = null;
+        $payable_type = null;
+        $payable_id = null;
+        $name = null;
+        $amount = null;
+        $email = $user->gt_email;
+        
+        if ($request->method() == "POST") {
+            $this->validate($request, [
+                'payable_type' => 'required|string',
+                'payable_id' => 'required|numeric'
+            ]);
+            $payable_type = $request->input('payable_type');
+            $payable_id = $request->input('payable_id');
+        } else {
+            //Assuming DuesTransaction for now
+            $payable = DuesTransaction::doesntHave('payment')->where('user_id', $user->id)->first();
+            if (!$payable) {
+                return response(view('errors.generic',
+                    ['error_code' => 400,
+                        'error_message' => 'No eligible Dues Transaction found for payment.']), 400);
+            }
+            $amount = $payable->package->cost;
+            $name = "Dues - " . $payable->package->name;
+            $email = $user->gt_email;
+        }
+        
+        if (!$payable) {
+            if ($payable_type == "App\DuesTransaction") {
+                $payable = DuesTransaction::find($payable_id);
+                $amount = $payable->package->amount;
+                $name = "Dues - " . $payable->package->name;
+                $email = $user->gt_email;
+            } elseif ($payable_type == "App\Event") {
+                $payable = Event::find($payable_id);
+                $amount = $payable->price;
+                $name = "Event - " . $payable->name;
+                $email = $user->gt_email;
+            } else {
+                return response()->json(['status' => 'error', 'error' => 'Invalid Payable Type'], 400);
+            }
+        }
+
+        $payment = new Payment();
+        $payment->amount = 0.00;
+        $payment->method = "square";
+        $payment->recorded_by = $user->id;
+        $payment->unique_id = bin2hex(openssl_random_pseudo_bytes(10));
+        $payment->notes = "Pending Square Payment";
+        $payable->payment()->save($payment);
+        
+        $squareResult = $this->createSquareCheckout($name, $amount, $email, $payment, true);
+        if (is_a($squareResult, "Illuminate\Http\RedirectResponse")) {
+            return $squareResult;
+        } else {
+            Log::error(get_class() . " - Error Creating Square Checkout - $squareResult");
+            return response(view('errors.generic',
+                ['error_code' => 500,
+                    'error_message' => 'Unable to process Square Checkout request.']), 500);
         }
     }
 
@@ -131,5 +207,82 @@ class PaymentController extends Controller
             return response()->json(['status' => 'error',
                 'message' => 'Payment does not exist or was previously deleted.'], 422);
         }
+    }
+
+    /**
+     * Creates Square Checkout Payment Flow
+     *
+     * @param $name string Name of line item
+     * @param $amount integer Amount in *whole* dollars to be paid (excluding fees!)
+     * @param $email string Email address for Square Receipt
+     * @param $payment \App\Payment Payment Model
+     * @param $addFee boolean Adds $3.00 transaction fee if true
+     */
+    public function createSquareCheckout($name, $amount, $email, $payment, $addFee)
+    {
+        $api = new CheckoutApi();
+        $location = (\App::environment('production')) ?
+            config('payment.square.location_id') :
+            config('payment.square.location_id_test');
+        $token = (\App::environment('production')) ?
+            config('payment.square.token') :
+            config('payment.square.token_test');
+
+        $line_items = [
+            [
+                "name" => ($name) ?: "Miscellaneous Payment",
+                "quantity" => "1",
+                "base_price_money" => [
+                    "amount" => (int) $amount * 100,
+                    "currency" => "USD"
+                ]
+            ]
+        ];
+        
+        if ($addFee) {
+            $line_items[] =
+            [
+                "name" => "Transaction Fee",
+                "quantity" => "1",
+                "base_price_money" => [
+                    "amount" => 300,
+                    "currency" => "USD"
+                ]
+            ];
+        }
+        
+        $order = new CreateOrderRequest([
+            "reference_id" => "PMT$payment->id",
+            "line_items" => $line_items
+        ]);
+        
+        $checkout_request = new CreateCheckoutRequest([
+            "idempotency_key" => $payment->unique_id,
+            "order" => $order,
+            "merchant_support_email" => "treasurer@robojackets.org",
+            "pre_populate_buyer_email" => $email,
+//            "redirect_url" => route('payments.complete')
+        ]);
+
+        try {
+            Configuration::getDefaultConfiguration()->setAccessToken($token);
+            $checkout = $api->createCheckout($location, $checkout_request);
+        } catch (\Exception $e) {
+            $message = $e->getMessage();
+            return $message;
+        }
+        
+        if ($checkout) {
+            $payment->checkout_id = $checkout['checkout']['id'];
+            return redirect($checkout['checkout']['checkout_page_url']);
+        }
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function handleSquareResponse(Request $request)
+    {
+        //
     }
 }
