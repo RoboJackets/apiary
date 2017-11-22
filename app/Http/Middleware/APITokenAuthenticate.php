baf62d8c81857969d04d419c0acf63863d1412b3
Ross Lindsay <ross@rosslindsay.com>
2017-11-21 21:58:07 -0500

    New middleware to handle API token authentication in a more friendly manner that doesn't reject you if you don't have a token and passes the request onto the next middleware (or whatever's next)

---
+++ app/Http/Middleware/APITokenAuthenticate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Factory as AuthFactory;
+use Illuminate\Support\Facades\Auth;
+
+class APITokenAuthenticate
+{
+    protected $auth;
+
+    public function __construct(AuthFactory $auth)
+    {
+        $this->auth = $auth;
+    }
+    
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = Auth::guard('api')->getTokenForRequest();
+        $hasToken = Auth::guard('api')->validate(['api_token' => $token]);
+        if ($hasToken) {
+            $this->auth->shouldUse('api');
+            $this->auth->authenticate();
+        }
+        return $next($request);
+    }
+}
