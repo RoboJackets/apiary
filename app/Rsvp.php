2ffeabaf3b5a2b998ed402356f09689570c7c252
Ross Lindsay <ross@rosslindsay.com>
2017-12-04 22:56:40 -0500

    Fix #190 - Adds soft delete trait to each model, add migrations to add deleted_at column

--- app/Rsvp.php
+++ app/Rsvp.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Rsvp extends Model
 {
+    use SoftDeletes;
+    
     /**
      * The attributes that are not mass assignable.
      *
