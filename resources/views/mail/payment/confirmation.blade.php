0e642a5de97419fc99f11e107a5c9327013ff2bc
Ross Lindsay <ross@rosslindsay.com>
2018-01-17 15:55:27 -0500

    Extraneous Spaces

--- resources/views/mail/payment/confirmation.blade.php
+++ resources/views/mail/payment/confirmation.blade.php
@@ -3,9 +3,9 @@
 # We've received your payment!
 
 @if($payment->method == "square")
-    We have received your payment of **{{$payment->amount}}** via **{{$payment->method_presentation}}** for **{{$payment->payable->for->name}}**.
+We have received your payment of **{{$payment->amount}}** via **{{$payment->method_presentation}}** for **{{$payment->payable->for->name}}**.
 @else
-    **{{$payment->user->name}}** accepted your payment of **{{$payment->amount}}** via **{{$payment->method_presentation}}** for **{{$payment->payable->for->name}}**.
+**{{$payment->user->name}}** accepted your payment of **{{$payment->amount}}** via **{{$payment->method_presentation}}** for **{{$payment->payable->for->name}}**.
 @endif
 This email constitutes a receipt - please keep it just in case!
 
