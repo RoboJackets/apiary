b51f26695f4e120135de7c0431ce6031e37f1d0c
Ross Lindsay <ross@rosslindsay.com>
2018-02-06 21:22:12 -0500

    #274 - Add payment failure verbiage

--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -13,7 +13,10 @@
     <div class="row">
         <div class="col-md-12">
             Something went wrong. Please try your request again later. If you continue to receive this error,
-            please contact #it-helpdesk on <a href="https://robojackets.slack.com">Slack</a>.
+            please contact #it-helpdesk on <a href="https://robojackets.slack.com">Slack</a>.<br/>
+            If you submitted payment information before reaching this screen, try refreshing this page once. 
+            If you continue to receive this error, please contact the treasurer to verify the status of your payment.
+            You should not re-enter your card information unless instructed.
         </div>
     </div>
     <hr>
