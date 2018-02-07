50ce06244f62ff436f681fc12b946d1fb7792554
Kristaps Berzinch <kristapsberzinch@gmail.com>
2018-02-06 21:34:23 -0500

    Update generic.blade.php

--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -13,8 +13,8 @@
     <div class="row">
         <div class="col-md-12">
             Something went wrong. Please try your request again later. If you continue to receive this error,
-            please contact #it-helpdesk on <a href="https://robojackets.slack.com">Slack</a>.<br/>
-            If you submitted payment information before reaching this screen, try refreshing this page once. 
+            please contact #it-helpdesk on <a href="https://robojackets.slack.com">Slack</a>.<br/><br>
+            <strong>If you submitted payment information before reaching this screen, try refreshing this page once.</strong> 
             If you continue to receive this error, please contact the treasurer to verify the status of your payment.
             You should not re-enter your card information unless instructed.
         </div>
@@ -40,7 +40,7 @@
             <b>User:</b> {{ cas()->user() }}
         </div>
         @endif
-        <div class="col-md-3">
+        <div class="col-md-6">
             <b>Time:</b> {{ date("D M j Y G:i:s T") }}
         </div>
     </div>
