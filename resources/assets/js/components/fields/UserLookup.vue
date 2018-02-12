940217f9e971bba558ca872964d076c131357970
RossIV <ross@rosslindsay.com>
2018-02-11 23:47:42 -0500

    #123 User lookup component works! Added change function to emit a value for use in parent; Added filter function for later tweaking of displayed search results; Fixed debounce (thanks stackoverflow); Added value prop for setting initial value (ex. edit screens)

--- resources/assets/js/components/fields/UserLookup.vue
+++ resources/assets/js/components/fields/UserLookup.vue
@@ -2,8 +2,11 @@
     <v-select
             label="name"
             :on-search="getOptions"
-            :options="this.userOptions"
-            placeholder="Enter a Username, GTID#, or First Name">
+            :on-change="change"
+            :filter="filter"
+            :options="this.options"
+            placeholder="Enter a First Name"
+            :value="value">
     </v-select>
 </template>
 
@@ -13,27 +16,41 @@
             return {
                 dataUrl: "",
                 baseUrl: "/api/v1/users/search?keyword=",
-                userOptions: [],
+                options: [],
+                searchKeyword: ""
+            }
+        },
+        props: {
+            value: {
+                type: [String, Object]
             }
         },
         methods: {
             getOptions(search, loading) {
                 loading(true);
-                console.log("options");
-                this.searchUsers(loading, search);
+                this.search(loading, search);
             },
-            searchUsers: _.debounce((loading, search) => {
-                console.log('searching');
+            search: _.debounce(function(loading, search) {
                 axios.get(this.baseUrl + encodeURIComponent(search))
                     .then(response => {
                         loading(false);
-                        this.userOptions = response.user.options;
+                        this.options = response.data.users;
                     })
                     .catch(response => {
                         console.log(response);
                         sweetAlert("Connection Error", "Unable to load data. Check your internet connection or try refreshing the page.", "error");
                     });
             }, 350),
+            change(value) {
+                if (value instanceof Object) {
+                    this.$emit('input', value);
+                }
+            },
+            //This controls which results show up in the dropdown after results are returned
+            //TODO: Make necessary modifications to allow search by username/GTID, then update placeholder
+            filter(option, label, search) {
+                return (label || '').toLowerCase().indexOf(search.toLowerCase()) > -1
+            }
         }
     }
 </script>
