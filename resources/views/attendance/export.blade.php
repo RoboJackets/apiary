98af131655b909a412ea0bba44950aa89e8a5649
RossIV <ross@rosslindsay.com>
2018-02-21 22:50:34 -0500

    Add route, base blade view, navigation entry for attendance export

---
+++ resources/views/attendance/export.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts/app')
+
+@section('title')
+    Attendance Export | {{ config('app.name') }}
+@endsection
+
+@section('content')
+    @component('layouts/title')
+        Attendance Export
+    @endcomponent
+
+    something something dark side something
+
+@endsection
