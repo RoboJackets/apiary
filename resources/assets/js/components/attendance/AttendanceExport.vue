b457c37739cd8644cf682a8678d7f653d2e029d8
Ross Lindsay <ross@rosslindsay.com>
2018-03-10 23:14:26 -0500

    Add submit button, data table, attendance load function

--- resources/assets/js/components/attendance/AttendanceExport.vue
+++ resources/assets/js/components/attendance/AttendanceExport.vue
@@ -38,11 +38,22 @@
                 </flat-pickr>
             </div>
         </div>
+        <div class="row form-row">
+            <button type="button" class="btn btn-primary" v-on:click="loadAttendance">Submit</button>
+        </div>
+        <hr>
+        <div id="attendance-table-div">
+            <datatable id="attendance-export-table"
+                       :data-object="attendanceTableData"
+                       :columns="attendanceTableConfig">
+            </datatable>
+        </div>
     </div>
 </template>
 
 <script>
     import { required, numeric } from 'vuelidate/lib/validators'
+    import moment from 'moment'
     export default {
         name: "attendance-export",
         data() {
@@ -61,6 +72,46 @@
                     enableTime:false,
                     altInput: true
                 },
+                attendanceTableData: [],
+                attendanceTableConfig: [
+                    {
+                        'title': 'Date', 'data': null, 'render': function (data, type, row) {
+                            try {
+                                let date = data.created_at;
+                                return date.substr(0,date.indexOf(' '));
+                            } catch (e) {
+                                return "";
+                            }
+                        }
+                    },
+                    {
+                        'title': 'Last Name', 'data': null, 'render': function (data, type, row) {
+                            try {
+                                return data.attendee.last_name;
+                            } catch (e) {
+                                return "Non-member";
+                            }
+                        }
+                    },
+                    {
+                        'title': 'First Name', 'data': null, 'render': function (data, type, row) {
+                            try {
+                                return data.attendee.first_name;
+                            } catch (e) {
+                                return "";
+                            }
+                        }
+                    },
+                    {
+                        'title': 'GTID', 'data': null, 'render': function (data, type, row) {
+                            try {
+                                return data.gtid;
+                            } catch (e) {
+                                return "";
+                            }
+                        }
+                    },
+                ],
             }
         },
         methods: {
@@ -91,6 +142,36 @@
                         }
                     });
             },
+            loadAttendance() {
+                let self = this;
+                axios.post(this.attendanceBaseUrl + "/search", this.attendance)
+                    .then(response => {
+                        self.attendanceTableData = response.data.attendance;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        swal("Error", "Unable to process data. Check your internet connection or try refreshing the page.", "error");
+                    });
+
+                //Sort the table by date then by last name per CoC request
+                let table = $('#attendance-export-table').DataTable();
+                table.order([0, 'asc'], [1, 'asc']);
+
+                //Set the export file name (MAGIC!)
+                let team_name = "core";
+                let end_date = (this.attendance.end_date != "") ? this.attendance.end_date : moment().format("YYYY-MM-DD");
+                let file_name = team_name + "_" + this.attendance.start_date + "_" + end_date;
+                console.log(file_name);
+                new $.fn.dataTable.Buttons(table, {
+                    buttons: [
+                        'copy', 'excel', 'print', {
+                            extend: 'csv',
+                            text: 'CSV',
+                            filename: file_name
+                        }
+                    ]
+                });
+            }
         },
         mounted() {
             this.loadTeams();
