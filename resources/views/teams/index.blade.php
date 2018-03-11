a595c7682c7b4a816c0063247f60f426fdbad484
Ross Lindsay <ross@rosslindsay.com>
2018-03-10 19:09:54 -0500

    Use data from controller instead of querying in the view; Lowercase team name for detail page

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -9,13 +9,13 @@
         Teams
     @endcomponent
     <div class="row">
-        @foreach (\App\Team::visible()->orderBy('name', 'asc')->get() as $team)
+        @foreach ($teams as $team)
             <div class="col-xs-12 col-sm-6 col-md-4">
                 <div class="card card-teams">
                     <div class="card-body">
                         <h5 class="card-title"><b>{{ $team->name }}</b></h5>
                         <p class="card-text">{{ $team->short_description }}</p>
-                        <a href="{{ route('teams.show', $team->id) }}" class="btn btn-primary">More Info</a>
+                        <a href="{{ route('teams.show', strtolower($team->name)) }}" class="btn btn-primary">More Info</a>
                     </div>
                 </div>
             </div>
