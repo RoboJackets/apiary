daf6b6232882799c702b049f5f34aec42470d6a9
Ross Lindsay <ross@rosslindsay.com>
2018-05-09 22:56:10 -0400

    Move the button into the footer; Make the cards a uniform size (more or less)

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -10,16 +10,14 @@
     @endcomponent
     <div class="row">
         @foreach ($teams as $team)
-            <div class="col-xs-12 col-sm-6 col-md-4">
-                <div class="card card-teams">
+            <div class="col-sm-12 col-md-6 col-lg-4 card-teams">
+                <div class="card mb-3 h-100">
                     <div class="card-body">
-                        <h5 class="card-title"><b><a href="{{ route('teams.show', strtolower($team->name)) }}">
-                                    {{$team->name}}
-                                </a></b></h5>
+                        <h5 class="card-title"><b>{{$team->name}}</b></h5>
                         <p class="card-text">{{ $team->description }}</p>
-                        <a href="{{ route('teams.show', strtolower($team->name)) }}" class="btn btn-primary">
-                            More Info
-                        </a>
+                    </div>
+                    <div class="card-footer">
+                        <team-membership-button :team="{{ $team }}" :user="{{ $user }}"></team-membership-button>
                     </div>
                 </div>
             </div>
