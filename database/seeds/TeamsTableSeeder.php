828863d7bd6473e326b2a170a2497588717b75d2
Ross Lindsay <ross@rosslindsay.com>
2018-05-28 22:26:58 -0400

    Update seeder to reflect current attributes

--- database/seeds/TeamsTableSeeder.php
+++ database/seeds/TeamsTableSeeder.php
@@ -12,18 +12,18 @@ class TeamsTableSeeder extends Seeder
     public function run()
     {
         DB::table('teams')->insert([
-            ['name' => 'IGVC', 'hidden' => false, 'attendable' => true, 'founding_year' => 2004,
-                'description' => '', 'slug' => 'igvc', ],
-            ['name' => 'BattleBots', 'hidden' => false, 'attendable' => true, 'founding_year' => 1999,
-                'description' => '', 'slug' => 'battlebots', ],
-            ['name' => 'Outreach', 'hidden' => false, 'attendable' => true, 'founding_year' => 2001,
-                'description' => '', 'slug' => 'outreach', ],
-            ['name' => 'RoboCup', 'hidden' => false, 'attendable' => true, 'founding_year' => 2007,
-                'description' => '', 'slug' => 'robocup', ],
-            ['name' => 'RoboRacing', 'hidden' => false, 'attendable' => true, 'founding_year' => 2013,
-                'description' => '', 'slug' => 'roboracing', ],
+            ['name' => 'IGVC', 'visible' => true, 'attendable' => true, 'founding_year' => 2004,
+                'description' => '', 'slug' => 'igvc', 'self_serviceable' => true, ],
+            ['name' => 'BattleBots', 'visible' => true, 'attendable' => true, 'founding_year' => 1999,
+                'description' => '', 'slug' => 'battlebots', 'self_serviceable' => true, ],
+            ['name' => 'Outreach', 'visible' => true, 'attendable' => true, 'founding_year' => 2001,
+                'description' => '', 'slug' => 'outreach', 'self_serviceable' => true, ],
+            ['name' => 'RoboCup', 'visible' => true, 'attendable' => true, 'founding_year' => 2007,
+                'description' => '', 'slug' => 'robocup', 'self_serviceable' => true, ],
+            ['name' => 'RoboRacing', 'visible' => true, 'attendable' => true, 'founding_year' => 2013,
+                'description' => '', 'slug' => 'roboracing', 'self_serviceable' => true, ],
             ['name' => 'Core', 'hidden' => true, 'attendable' => true, 'founding_year' => 1969,
-                'description' => '', 'slug' => 'core', ],
+                'description' => '', 'slug' => 'core', 'self_serviceable' => false, ],
         ]);
     }
 }
