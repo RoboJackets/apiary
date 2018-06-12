ae7536c81bb20370dbb4ac9cae70e7ebc71b6e9d
Ross Lindsay <ross@rosslindsay.com>
2018-06-12 19:22:32 -0400

    Per conversation in Slack, add Slack and Sympa links to team cards

---
+++ database/migrations/2018_06_12_190633_add_sympa_and_slack_links_to_teams.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSympaAndSlackLinksToTeams extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('slack_channel_name')->after('description');
+            $table->string('slack_channel_id')->after('description');
+            $table->string('mailing_list_name')->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropColumn('slack_channel_name');
+            $table->dropColumn('slack_channel_id');
+            $table->dropColumn('mailing_list_name');
+        });
+    }
+}
