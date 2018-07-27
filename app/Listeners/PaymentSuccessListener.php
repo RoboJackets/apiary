bd8e5fdc0a3f56d6ce72eea0457009d9b18dd137
Ross Lindsay <ross@rosslindsay.com>
2018-07-26 22:42:38 -0400

    Fixed some incorrect conditionals; Cleanup

--- app/Listeners/PaymentSuccessListener.php
+++ app/Listeners/PaymentSuccessListener.php
@@ -31,20 +31,22 @@ class PaymentSuccessListener
     {
         $payment = $event->payment;
         $payable = $payment->payable;
-        Log::info(get_class() . ": Handling successful payment ID " . $payment->id);
+        Log::info(get_class() . ": Handling successful payment ID $payment->id");
         // If this is a Dues Transaction payment, update user roles
         if ($payable instanceof DuesTransaction) {
             if ($payable->status == "paid") {
                 $user = $payable->user;
-                Log::info(get_class() . ": Updating role membership for " . $user->uid);
+                Log::info(get_class() . ": Updating role membership for $user->uid");
                 if ($user->hasRole('non-member')) {
                     $user->removeRole('non-member');
                 }
                 $role_member = Role::where('name', 'member')->first();
-                if ($role_member) {
+                if ($role_member && !$user->hasRole('member')) {
                     $user->assignRole($role_member);
+                } elseif ($user->hasRole('member')) {
+                    Log::warning(get_class() . ": Role 'member' already assigned to $user->uid");
                 } else {
-                    Log::error(get_class()."Role 'member' not found for assignment to $user->uid.");
+                    Log::error(get_class() . ": Role 'member' not found for assignment to $user->uid");
                 }
             }
         }
