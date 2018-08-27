a058cdf9e5f3cc6301142e30835d7266b25887ae
RossIV <ross@rosslindsay.com>
2018-08-26 22:01:14 -0400

    Add migration for email notification templates

---
+++ database/migrations/2018_08_24_230328_create_notification_templates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification_templates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('subject');
+            $table->longText('body_markdown')->nullable();
+
+            $table->unsignedInteger('created_by');
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification_templates');
+    }
+}
