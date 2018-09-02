5577bc45c265f37a77cfb67154b8b38236c4e672
Kristaps Berzinch <kristaps@gatech.edu>
2018-09-02 19:37:59 -0400

    Initial Nova configuration for user management

---
+++ app/Nova/Filters/UserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class UserType extends Filter
+{
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->role($value);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return ['Administrator' => 'admin', 'Officer I' => 'officer-i', 'Officer II' => 'officer-ii', 'Core' => 'core', 'Member' => 'member', 'Non-Member' => 'non-member'];
+    }
+}
--- post-deploy-hook.sh
+++ post-deploy-hook.sh
@@ -7,6 +7,7 @@ php artisan migrate --no-interaction
 php artisan config:clear --no-interaction
 php artisan view:clear --no-interaction
 php artisan route:clear --no-interaction
+php artisan nova:publish --no-interaction
 npm install --no-progress
 npm run production --no-progress
 php artisan up
