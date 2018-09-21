1d3ea409ac049b815cd42ea600ace792eb989f71
Kristaps Berzinch <kristaps@gatech.edu>
2018-09-20 20:50:28 -0400

    Style

--- app/Nova/RecruitingVisit.php
+++ app/Nova/RecruitingVisit.php
@@ -2,14 +2,11 @@
 
 namespace App\Nova;
 
-use Laravel\Nova\Fields\ID;
-use Illuminate\Http\Request;
-use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Panel;
+use Illuminate\Http\Request;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\BelongsTo;
 
 class RecruitingVisit extends Resource
 {
