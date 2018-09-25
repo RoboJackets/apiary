cd5b669453ffbc7b22562a9397e738b9e87500df
Josh Oldenburg <josh.oldenburg@me.com>
2018-09-24 23:43:09 -0400

    Change last step Continue button to say Submit

--- resources/assets/js/components/dues/Demographics.vue
+++ resources/assets/js/components/dues/Demographics.vue
@@ -26,7 +26,7 @@
             <button @click.prevent="$emit('back')" class="btn btn-secondary float-left">Back</button>
           </div>
           <div class="col-lg-3 col-6">
-            <button type="submit" class="btn btn-primary float-right">Continue</button>
+            <button type="submit" class="btn btn-primary float-right">Submit</button>
             <button @click.prevent="$emit('next')" class="btn btn-secondary float-right mx-2">Skip</button>
           </div>
         </div>
