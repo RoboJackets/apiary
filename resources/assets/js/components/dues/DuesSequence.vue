cde05d1e5ada74731e0e6d874c9dfe645dd6313d
Josh Oldenburg <josh.oldenburg@me.com>
2018-09-26 21:24:27 -0400

    Switch to using v-show instead of v-if
    This will let the components keep their values when going back.

--- resources/assets/js/components/dues/DuesSequence.vue
+++ resources/assets/js/components/dues/DuesSequence.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <div v-if="currentStepName == 'dues-required-info'">
+    <div v-show="currentStepName == 'dues-required-info'">
       <dues-required-info :user="user" @next="next" @back="back">
       </dues-required-info>
     </div>
 
-    <div v-if="currentStepName == 'safety-agreement'">
+    <div v-show="currentStepName == 'safety-agreement'">
       <safety-agreement :user-uid="user.uid" @next="next" @back="back"></safety-agreement>
     </div>
 
-    <div v-if="currentStepName == 'dues-additional-info'">
+    <div v-show="currentStepName == 'dues-additional-info'">
       <dues-additional-info :user="user" @next="next" @back="back"></dues-additional-info>
     </div>
 
-    <div v-if="currentStepName == 'dues-demographics-info'">
+    <div v-show="currentStepName == 'dues-demographics-info'">
       <demographics :user="user" @next="next" @back="back"></demographics>
     </div>
 
-    <div v-if="currentStepName == 'dues-payment-instructions'">
+    <div v-show="currentStepName == 'dues-payment-instructions'">
       <payment-instructions>
         <p>
           To complete your annual registration, please submit the SGA-mandated dues payments. RoboJackets dues are $100 for the year or $55 for the semester.
