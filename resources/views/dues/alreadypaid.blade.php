89a868f14281df93ac041f3c6932e6a734871678
Josh Oldenburg <josh.oldenburg@me.com>
2018-10-01 18:41:37 -0400

    Send people to treasurer@robojackets.org

--- resources/views/dues/alreadypaid.blade.php
+++ resources/views/dues/alreadypaid.blade.php
@@ -13,7 +13,8 @@
     <div class="row">
         <div class="col-md-12">
             We have you on record as having already paid dues for this semester. You do not need to pay again. If you
-            believe you haven't paid, please contact #it-helpdesk on <a href="https://robojackets.slack.com">Slack</a>.
+            believe you haven't paid, please email
+                <a href="mailto:treasurer@robojackets.org">treasurer@robojackets.org</a>.
         </div>
     </div>
 
