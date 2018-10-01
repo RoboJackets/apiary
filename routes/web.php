5c13316562925ac3be10a0046789ff1dcee9c936
Josh Oldenburg <josh.oldenburg@me.com>
2018-10-01 17:34:59 -0400

    Return a proper error page when active members view the dues flow

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,10 @@ Route::group(['middleware' => 'auth.cas.force'], function () {
     Route::prefix('dues')->group(function () {
         Route::get('/', function () {
             if (auth()->user()->is_active) {
-                return redirect()->route('home');
+                return response()->view('errors.generic', [
+                    'error_code' => 400,
+                    'error_message' => 'You\'ve already paid dues for this semester',
+                ], 400);
             } else {
                 return view('dues/payDues');
             }
