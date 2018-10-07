9781043da1f67b47b378a1ff0a00fc8322aa4698
Ross Lindsay <ross@rosslindsay.com>
2018-10-06 22:56:03 -0400

    Create API Resources for models

---
+++ app/Http/Resources/DuesTransaction.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Payment as PaymentResource;
+use App\Http\Resources\DuesPackage as DuesPackageResource;
+
+class DuesTransaction extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'dues_package_id' => $this->dues_package_id,
+            'status' => $this->status,
+            'swag_shirt_provided' => $this->swag_shirt_provided,
+            'swag_shirt_providedBy' => $this->swag_shirt_providedBy,
+            'swag_shirt_status' => $this->swag_shirt_status,
+            'swag_polo_provided' => $this->swag_polo_provided,
+            'swag_polo_providedBy' => $this->swag_polo_providedBy,
+            'swag_polo_status' => $this->swag_polo_status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+
+            // Relationships
+            'package' => DuesPackageResource::collection($this->whenLoaded('package')),
+            'payment' => PaymentResource::collection($this->whenLoaded('payment')),
+        ];
+    }
+}
--- app/Http/Resources/Event.php
+++ app/Http/Resources/Event.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\User as UserResource;
 
 class Event extends JsonResource
 {
@@ -14,6 +15,18 @@ class Event extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'cost' => $this->cost,
+            'allow_anonymous_rsvp' => (bool) $this->allow_anonymous_rsvp,
+            'location' => $this->location,
+            'start_time' => $this->start_time,
+            'end_time' => $this->end_time,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+            'organizer' => new UserResource($this->whenLoaded('organizer'))
+        ];
     }
 }
