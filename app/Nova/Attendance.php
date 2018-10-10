ced7f07dc03651848baf4b3e4b7ba9d70ad7a2fa
Josh Oldenburg <josh.oldenburg@me.com>
2018-10-10 15:54:29 -0400

    Add Attendance resource to Nova

---
+++ app/Nova/Attendance.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Panel;
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Fields\Currency;
+use Laravel\Nova\Fields\MorphTo;
+
+class Attendance extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Attendance';
+
+    /**
+     * Get the displayble label of the resource.
+     *
+     * @return string
+     */
+    public static function label()
+    {
+        return 'Attendance';
+    }
+
+    /**
+     * Get the displayble singular label of the resource.
+     *
+     * @return string
+     */
+    public static function singularLabel()
+    {
+        return 'Attendance Record';
+    }
+
+    /**
+     * Get the URI key for the resource.
+     *
+     * @return string
+     */
+    public static function uriKey()
+    {
+        return 'attendance';
+    }
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            new Panel('Basic Information', $this->basicFields()),
+
+            new Panel('Metadata', $this->metaFields()),
+        ];
+    }
+
+    protected function basicFields()
+    {
+        return [
+            Text::make('GTID')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            (new BelongsTo('User', 'attendee', 'App\Nova\User'))
+                ->sortable(),
+
+            (new MorphTo('Attended', 'attendable'))
+                ->sortable()
+                ->types([
+                    Event::class,
+                    Team::class,
+                ]),
+
+            (new BelongsTo('Recorded By', 'recorded', 'App\Nova\User'))
+                ->sortable()
+                ->help('The user that recorded the swipe'),
+
+            DateTime::make('Time', 'created_at')
+                ->sortable(),
+
+            Text::make('Source')
+                ->hideFromIndex()
+                ->sortable(),
+        ];
+    }
+
+    protected function metaFields()
+    {
+        return [
+            DateTime::make('Created', 'created_at')
+                ->onlyOnDetail(),
+
+            DateTime::make('Last Updated', 'updated_at')
+                ->onlyOnDetail(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
