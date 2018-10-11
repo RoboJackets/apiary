09bbf28cb25633c35ead9ad629942f0b317e0977
Daniel Hsing <hsing.daniel@gmail.com>
2018-10-11 15:28:09 -0400

    feat(migration): add migration that gives 'update-teams-membership-own' perms to non-members
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

---
+++ database/migrations/2018_10_11_152147_add_hidden_teams_permission_to_non_member.php
@@ -0,0 +1,40 @@
+<?php
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Migrations\Migration;
+
+class AddHiddenTeamsPermissionToNonMember extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spat5ie.permission.cache');
+
+        Permission::firstOrCreate(['name' => 'update-teams-membership-own']);
+
+        $role = Role::firstOrCreate(['name' => 'non-member']);
+        $role->givePermissionTo('update-teams-membership-own');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        $role = Role::firstOrCreate(['name' => 'non-member']);
+        $role->revokePermissionTo('update-teams-membership-own');
+
+        Permission::where('name', 'update-teams-membership-own')->delete();
+    }
+}
