7d77d563ffd627ebc4805565160ae26d3a86a939
RossIV <ross@rosslindsay.com>
2018-10-18 19:36:42 -0400

    Add dynamic includes and resource responses that I missed the first time around

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -7,10 +7,14 @@ use Bugsnag;
 use App\User;
 use App\Attendance;
 use Illuminate\Http\Request;
+use App\Traits\AuthorizeInclude;
 use Illuminate\Database\QueryException;
+use App\Http\Resources\Attendance as AttendanceResource;
 
 class AttendanceController extends Controller
 {
+    use AuthorizeInclude;
+
     public function __construct()
     {
         $this->middleware('permission:read-attendance', ['only' => ['index', 'search']]);
@@ -27,9 +31,10 @@ class AttendanceController extends Controller
      */
     public function index(Request $request)
     {
-        $attendance = Attendance::with('attendee')->get();
+        $include = $request->input('include');
+        $att = Attendance::with($this->authorizeInclude(Attendance::class, $include))->get();
 
-        return response()->json(['status' => 'success', 'attendance' => $attendance]);
+        return response()->json(['status' => 'success', 'attendance' => AttendanceResource::collection(($att))]);
     }
 
     /**
@@ -83,7 +88,7 @@ class AttendanceController extends Controller
             $att = array_merge($att->toArray(), $name);
         }
 
-        return response()->json(['status' => 'success', 'attendance' => $att], $code);
+        return response()->json(['status' => 'success', 'attendance' => new AttendanceResource($att)], $code);
     }
 
     /**
@@ -95,10 +100,11 @@ class AttendanceController extends Controller
      */
     public function show(Request $request, $id)
     {
+        $include = $request->input('include');
         $user = auth()->user();
-        $att = Attendance::find($id);
+        $att = Attendance::with($this->authorizeInclude(Attendance::class, $include))->find($id);
         if ($att && ($att->gtid == $user->gtid || $user->can('read-attendance'))) {
-            return response()->json(['status' => 'success', 'attendance' => $att]);
+            return response()->json(['status' => 'success', 'attendance' => new AttendanceResource($att)]);
         } else {
             return response()->json(['status' => 'error', 'message' => 'Attendance not found.'], 404);
         }
@@ -112,6 +118,7 @@ class AttendanceController extends Controller
      */
     public function search(Request $request)
     {
+        $include = $request->input('include');
         $this->validate($request, [
             'attendable_type' => 'required',
             'attendable_id' => 'required|numeric',
@@ -122,10 +129,10 @@ class AttendanceController extends Controller
         $att = Attendance::where('attendable_type', '=', $request->input('attendable_type'))
             ->where('attendable_id', '=', $request->input('attendable_id'))
             ->start($request->input('start_date'))->end($request->input('end_date'))
-            ->with('attendee')->get();
+            ->with($this->authorizeInclude(Attendance::class, $include))->get();
 
         if ($att) {
-            return response()->json(['status' => 'success', 'attendance' => $att]);
+            return response()->json(['status' => 'success', 'attendance' => AttendanceResource::collection($att)]);
         } else {
             return response()->json(['status' => 'error', 'message' => 'Attendance not found.'], 404);
         }
@@ -159,7 +166,7 @@ class AttendanceController extends Controller
                 return response()->json(['status' => 'error', 'message' => $errorMessage], 500);
             }
 
-            return response()->json(['status' => 'success', 'attendance' => $att]);
+            return response()->json(['status' => 'success', 'attendance' => new AttendanceResource($att)]);
         } else {
             return response()->json(['status' => 'error', 'message' => 'Attendance not found.'], 404);
         }
