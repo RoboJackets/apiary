e915a827b34f9522db4046bbf2298eb2ab8df085
Josh Oldenburg <josh.oldenburg@me.com>
2018-10-26 16:11:47 -0400

    Use statistics endpoint for the attendance report

--- resources/assets/js/components/nova/AttendanceReport.vue
+++ resources/assets/js/components/nova/AttendanceReport.vue
@@ -3,11 +3,11 @@
     <heading class="mb-6">Attendance</heading>
 
     <div class="flex flex-wrap -mx-3 mb-3">
-      <ChartistCard title="Average Daily Members (All Teams)" class="w-full" total="123" total-suffix="per week" :data="data" :options="options" type="bar"/>
+      <ChartistCard title="Average Daily Members (All Teams)" class="w-full" :loading="loading" :total="data.averageWeeklyMembers" total-suffix="average total members per week" :data="data.averageDailyMembers" :options="dailyAverageBarGraphOptions" type="bar"/>
 
-      <ChartistCard title="BattleBots" class="w-1/2" :data="data" :options="options" type="line"/>
+      <ChartistCard class="w-1/2" :loading="loading" v-for="team in data.byTeam" :key="team.name" :data="team.data" :options="teamLineGraphOptions" :title="team.name" type="line"/>
 
-    <!-- <base-trend-metric :chart-data="data" title="fdsa" />-->
+      <ChartistCard class="w-full" :loading="loading" :data="data.events" :options="eventBarGraphOptions" title="Events" :total="data.totalEventAttendees" total-suffix="people attended an event" type="bar"/>
     </div>
   </div>
 </template>
@@ -16,6 +16,7 @@
 import Chartist from 'chartist';
 import ctPointLabels from 'chartist-plugin-pointlabels';
 import ChartistCard from './ChartistCard';
+// moment and lodash are available globally already, but Chartist is not
 
 export default {
   components: {
@@ -23,15 +24,12 @@ export default {
   },
   data() {
     return {
+      loading: true,
+      range: 52,
       data: {
-        labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-        series: [ [10, 12, 9, 7, 8, 5, 15] ],
       },
-      options: {
+      dailyAverageBarGraphOptions: {
         fullWidth: false,
-        chartPadding: {
-          // right: 40
-        },
         axisY: {
           low: 0,
         },
@@ -41,25 +39,128 @@ export default {
         plugins: [
           ctPointLabels({
             textAnchor: 'middle',
+            labelInterpolationFnc: function(value) {
+              return value.toFixed(1);
+            },
+          }),
+        ],
+      },
+      teamLineGraphOptions: {
+        lineSmooth: Chartist.Interpolation.none(),
+        showPoint: false,
+        axisX: {
+          type: Chartist.FixedScaleAxis,
+          divisor: 12,
+          showGrid: true,
+          labelInterpolationFnc: function(value) {
+            return moment(value).format('MMM D');
+          },
+        },
+        plugins: [],
+      },
+      eventBarGraphOptions: {
+        fullWidth: false,
+        axisY: {
+          low: 0,
+          onlyInteger: true,
+        },
+        axisX: {
+          showGrid: false,
+        },
+        plugins: [
+          ctPointLabels({
+            textAnchor: 'middle',
+            labelInterpolationFnc: function(value) {
+              return value.toFixed(0);
+            },
           }),
         ],
       },
     };
   },
   mounted() {
-    new Chartist.Bar(this.$refs.chart2, {
-      labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-      series: [ [8, 9, 16, 7, 8, 9, 12] ],
-    }, {
-      fullWidth: false,
-      chartPadding: {
-        // right: 40
-      }
-    });
+    this.getData(this.range);
+  },
+  methods: {
+    getData(range) {
+      this.loading = true;
+      Nova.request().get('/api/v1/attendance/statistics?range=' + range).then(response => {
+        var stats = response.data.statistics;
+
+        var averageDailyMembersLabels = [];
+        var averageDailyMembersSeries = [];
+        for (var label in stats.averageDailyMembers) {
+          averageDailyMembersLabels.push(label);
+          averageDailyMembersSeries.push(stats.averageDailyMembers[label]);
+        }
+
+        var eventLabels = [];
+        var eventSeries = [];
+        for (var label in stats.events) {
+          eventLabels.push(label);
+          eventSeries.push(stats.events[label]);
+        }
+
+        var formatted = {
+          averageDailyMembers: {
+            labels: averageDailyMembersLabels,
+            series: [averageDailyMembersSeries],
+          },
+          averageWeeklyMembers: stats.averageWeeklyMembers.toFixed(1),
+          byTeam: [],
+          events: {
+            labels: eventLabels,
+            series: [eventSeries],
+          },
+          totalEventAttendees: stats.eventAttendeeTotal,
+        };
+
+        for (var teamName in stats.byTeam) {
+          var teamData = stats.byTeam[teamName];
+          var team = {
+            name: teamName,
+            data: {
+              series: [],
+            },
+          };
+
+          // Used in a loop below, so only load this once
+          var currentYear = moment().year();
+
+          // Create one series per year, so group by year and then map each to a series object
+          var series = _.chain(teamData).toPairs().groupBy(function(row) {
+            return row[0].substr(0, 4);
+          }).map(function(yearData, year) {
+            // If there are missing week data points in the year, fill them in with zeros
+            if (yearData.length < 52) { // FIXME
+              for (var i = 1; i <= 52; i++) {
+                var weekName = year + ' ' + i;
+                // Skip if that week already has a row (search by the zeroth index being equal to the week name)
+                if (_.some(yearData, [0, weekName])) continue;
+                else yearData.splice(i - 1, 0, [weekName, 0]);
+              }
+            }
+            return {
+              name: year,
+              data: _.map(yearData, function(row) {
+                // Parse the year given so the date is set correctly, but then change it to the current year so the
+                // chart series are overlapping
+                return {
+                  x: moment(row[0], 'GGGG W').year(currentYear).toDate(),
+                  y: row[1],
+                };
+              }),
+            };
+          }).reverse().value();
+          team.data.series = series;
+
+          formatted.byTeam.push(team);
+        }
+
+        this.data = formatted;
+        this.loading = false;
+      });
+    }
   },
 }
 </script>
-
-<style>
-/* Scoped Styles */
-</style>
--- resources/assets/js/components/nova/ChartistCard.vue
+++ resources/assets/js/components/nova/ChartistCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="px-3 mb-6">
-    <loading-card :loading="false" class="pt-4">
+    <loading-card :loading="loading" class="pt-4">
       <div class="flex mb-4 mx-6">
         <h3 class="mr-3 text-base text-80 font-bold">{{ title }}</h3>
-        <p v-if="weeklyTotal" class="ml-auto text-black text-xl font-bold">{{ weeklyTotal }}
+        <p v-if="total" class="ml-auto text-black text-xl font-bold">{{ total }}
           <span v-if="totalSuffix" class="ml-.5 text-sm text-80">{{ totalSuffix }}</span>
         </p>
       </div>
@@ -14,7 +14,6 @@
 
 <script>
 import Chartist from 'chartist';
-import ctPointLabels from 'chartist-plugin-pointlabels';
 
 export default {
   props: {
@@ -27,19 +26,34 @@ export default {
       type: String,
       required: true,
     },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: function() {
+    return {
+      chart: null,
+    };
   },
   mounted() {
     switch (this.type) {
-    case 'bar':
-      new Chartist.Bar(this.$refs.chart, this.data, this.options);
-      break;
-    case 'line':
-      new Chartist.Line(this.$refs.chart, this.data, this.options);
-      break;
-    default:
-      console.log('Invalid chart type ' + this.type);
-      break;
+      case 'bar':
+        this.chart = new Chartist.Bar(this.$refs.chart, this.data, this.options);
+        break;
+      case 'line':
+        this.chart = new Chartist.Line(this.$refs.chart, this.data, this.options);
+        break;
+      default:
+        console.log('Invalid chart type ' + this.type);
+        break;
     }
+  },
+  watch: {
+    data: function(val) {
+      console.log(this.data);
+      this.chart.update(val);
+    },
   },
 }
 </script>
