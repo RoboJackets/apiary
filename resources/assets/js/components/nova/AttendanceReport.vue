381b423ad837e4910ca11677c6d5a178dc5c56cb
Josh Oldenburg <josh.oldenburg@me.com>
2018-10-28 10:29:09 -0400

    Add a range dropdown

--- resources/assets/js/components/nova/AttendanceReport.vue
+++ resources/assets/js/components/nova/AttendanceReport.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="attendancereport">
-    <heading class="mb-6">Attendance</heading>
-
     <div class="flex flex-wrap -mx-3 mb-3">
+      <heading class="mb-6 mx-3">Attendance</heading>
+      <select @change="newSelection" class="ml-auto min-w-24 h-6 text-xs no-appearance bg-white mx-3">
+        <option v-for="range in ranges" :key="range" :value="range" :selected="selectedRange == range">{{ range }} weeks</option>
+      </select>
+
       <ChartistCard :title="'Average Daily Members (All Teams, Last ' + selectedRange + ' Weeks)'" class="w-full" :loading="loading" :total="data.averageWeeklyMembers" total-suffix="average total members per week" :data="data.averageDailyMembers" :options="dailyAverageBarGraphOptions" type="bar"/>
 
       <ChartistCard class="w-1/2" :loading="loading" v-for="team in data.byTeam" :key="team.name" :data="team.data" :options="teamLineGraphOptions" :title="team.name" type="line" legend/>
 
-      <ChartistCard class="w-full" :loading="loading" :data="data.events" :options="eventBarGraphOptions" title="Events" :total="data.totalEventAttendees" total-suffix="people attended an event" type="bar"/>
+      <ChartistCard class="w-full" :loading="loading" :data="data.events" :options="eventBarGraphOptions" :title="'Events (Last ' + selectedRange + ' Weeks)'" :total="data.totalEventAttendees" total-suffix="different people attended an event" :fallback="'No events in the last ' + selectedRange + ' weeks'" type="bar" v-if="data.events.labels.length > 0"/>
+      <div class="px-3 mb-6 w-full" v-if="!loading && data.events.labels.length == 0">
+        <base-value-metric :loading="false" :title="'Events (Last ' + selectedRange + ' Weeks)'" :value="'No events in the last ' + selectedRange + ' weeks'" prefix=""/>
+      </div>
     </div>
   </div>
 </template>
@@ -77,15 +83,26 @@ export default {
           }),
         ],
       },
+      selectedRange: 52,
+      ranges: [
+        12,
+        26,
+        52,
+        104,
+      ],
     };
   },
   mounted() {
-    this.getData(this.range);
+    this.getData();
   },
   methods: {
-    getData(range) {
+    newSelection(event) {
+      this.selectedRange = event.target.value;
+      this.getData();
+    },
+    getData() {
       this.loading = true;
-      Nova.request().get('/api/v1/attendance/statistics?range=' + range).then(response => {
+      Nova.request().get('/api/v1/attendance/statistics?range=' + this.selectedRange).then(response => {
         var stats = response.data.statistics;
 
         var averageDailyMembersLabels = [];
