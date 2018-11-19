9b704e51ee3059d9045738f9d4d23c0f65306ed0
RossIV <ross@rosslindsay.com>
2018-11-18 21:31:03 -0500

    Formatting; Add down()

--- database/migrations/2018_11_01_205548_mass_update_roles_permissions.php
+++ database/migrations/2018_11_01_205548_mass_update_roles_permissions.php
@@ -48,34 +48,34 @@ class MassUpdateRolesPermissions extends Migration
 
         // And finally let's assign permissions to the new roles
         $r_member_perms = ['read-users-own', 'update-users-own', 'read-events', 'update-events-own',
-                            'create-rsvps-own', 'read-rsvps-own', 'update-rsvps-own', 'delete-rsvps-own',
-                            'create-payments-own', 'read-payments-own', 'read-dues-packages',
-                            'create-dues-transactions-own', 'read-dues-transactions-own', 'read-recruiting-visits-own',
-                            'update-recruiting-visits-own', 'delete-recruiting-visits-own', 'read-attendance-own',
-                            'read-teams', 'read-teams-membership-own', 'update-teams-membership-own', ];
+            'create-rsvps-own', 'read-rsvps-own', 'update-rsvps-own', 'delete-rsvps-own',
+            'create-payments-own', 'read-payments-own', 'read-dues-packages',
+            'create-dues-transactions-own', 'read-dues-transactions-own', 'read-recruiting-visits-own',
+            'update-recruiting-visits-own', 'delete-recruiting-visits-own', 'read-attendance-own',
+            'read-teams', 'read-teams-membership-own', 'update-teams-membership-own'];
         $r_member->syncPermissions($r_member_perms);
         $r_non_member->syncPermissions($r_member_perms);
 
         $r_trainer_perms = ['read-users', 'read-users-emergency_contact', 'create-attendance', 'read-attendance',
-                            'read-teams-hidden', ];
+            'read-teams-hidden'];
         $r_trainer->syncPermissions($r_trainer_perms);
 
         $r_tl_perms = ['read-users', 'read-users-emergency_contact', 'read-teams-membership'];
         $r_tl->syncPermissions($r_tl_perms);
 
         $r_pm_perms = ['read-users', 'read-users-emergency_contact', 'read-rsvps', 'create-payments',
-                        'create-payments-cash', 'create-payments-check', 'create-recruiting-visits',
-                        'create-attendance', 'read-attendance', 'read-teams-membership', 'update-teams', ];
+            'create-payments-cash', 'create-payments-check', 'create-recruiting-visits',
+            'create-attendance', 'read-attendance', 'read-teams-membership', 'update-teams'];
         $r_pm->syncPermissions($r_pm_perms);
 
         $r_officer_perms = ['read-users', 'read-users-gtid', 'read-users-emergency_contact', 'create-events',
-                            'update-events', 'create-rsvps', 'read-rsvps', 'delete-rsvps', 'create-payments',
-                            'create-payments-cash', 'create-payments-check', 'read-payments', 'create-dues-packages',
-                            'update-dues-packages', 'create-dues-transactions', 'read-dues-transactions',
-                            'update-dues-transactions', 'create-recruiting-visits', 'read-recruiting-visits',
-                            'create-recruiting-campaigns', 'read-recruiting-campaigns', 'update-recruiting-campaigns',
-                            'send-notifications', 'create-attendance', 'read-attendance', 'update-attendance',
-                            'create-teams', 'read-teams-membership', 'update-teams', 'read-teams-hidden', ];
+            'update-events', 'create-rsvps', 'read-rsvps', 'delete-rsvps', 'create-payments',
+            'create-payments-cash', 'create-payments-check', 'read-payments', 'create-dues-packages',
+            'update-dues-packages', 'create-dues-transactions', 'read-dues-transactions',
+            'update-dues-transactions', 'create-recruiting-visits', 'read-recruiting-visits',
+            'create-recruiting-campaigns', 'read-recruiting-campaigns', 'update-recruiting-campaigns',
+            'send-notifications', 'create-attendance', 'read-attendance', 'update-attendance',
+            'create-teams', 'read-teams-membership', 'update-teams', 'read-teams-hidden'];
         $r_officer->syncPermissions($r_officer_perms);
 
         $r_admin_perms = Permission::all();
@@ -91,5 +91,88 @@ class MassUpdateRolesPermissions extends Migration
     {
         // Reset cached roles and permissions
         app()['cache']->forget('spatie.permission.cache');
+
+        // Delete the roles that we aren't using any more
+        $r_officer = Role::where('name', 'officer')->delete();
+        $r_pm = Role::where('name', 'project-manager')->delete();
+        $r_tl = Role::where('name', 'team-lead')->delete();
+
+        // Re-create the old roles that we deleted earlier
+        $r_officer_ii = Role::firstOrCreate(['name' => 'officer-ii']);
+        $r_officer_i = Role::firstOrCreate(['name' => 'officer-i']);
+        $r_core = Role::firstOrCreate(['name' => 'core']);
+
+        // Fetch existing roles for mass update of permissions
+        $r_member = Role::firstOrCreate(['name' => 'member']);
+        $r_non_member = Role::firstOrCreate(['name' => 'non-member']);
+        $r_admin = Role::firstOrCreate(['name' => 'admin']);
+
+        // Delete newly-created permissions
+        $p_u_gtid = Permission::where('name', 'read-users-gtid')->delete();
+        $p_u_api = Permission::where('name', 'read-users-api_token')->delete();
+        $p_u_ec = Permission::where('name', 'read-users-emergency_contact')->delete();
+        $p_u_d = Permission::where('name', 'read-users-demographics')->delete();
+        $p_p_cash = Permission::where('name', 'create-payments-cash')->delete();
+        $p_p_check = Permission::where('name', 'create-payments-check')->delete();
+        $p_p_square = Permission::where('name', 'create-payments-square')->delete();
+        $p_p_sqc = Permission::where('name', 'create-payments-squarecash')->delete();
+        $p_p_swipe = Permission::where('name', 'create-payments-swipe')->delete();
+        $p_d_own = Permission::where('name', 'create-dues-transactions-own')->delete();
+        $p_t_mem = Permission::where('name', 'read-teams-membership')->delete();
+        $p_t_mem_own = Permission::where('name', 'read-teams-membership-own')->delete();
+
+        // Reassign permissions (yes this is gross)
+        $r_admin_perms = ['create-users', 'read-users', 'read-users-own', 'update-users', 'update-users-own',
+            'delete-users', 'create-events', 'read-events', 'update-events', 'update-events-own', 'delete-events',
+            'create-rsvp', 'create-rsvps-own', 'read-rsvps', 'read-rsvps-own', 'update-rsvps', 'update-rsvps-own',
+            'delete-rsvps', 'delete-rsvps-own', 'create-payments', 'create-payments-own', 'read-payments',
+            'read-payments-own', 'update-payments', 'delete-payments', 'create-dues-packages', 'read-dues-packages',
+            'update-dues-packages', 'delete-dues-packages', 'create-dues-transactions', 'read-dues-transactions',
+            'read-dues-transactions-own', 'update-dues-transactions', 'delete-dues-transactions',
+            'create-recruiting-visits', 'read-recruiting-visits', 'read-recruiting-visits-own',
+            'update-recruiting-visits', 'update-recruiting-visits-own', 'delete-recruiting-visits',
+            'delete-recruiting-visits-own', 'send-notifications', 'create-attendance', 'read-attendance',
+            'read-attendance-own', 'update-attendance', 'delete-attendance', 'create-teams', 'read-teams',
+            'update-teams', 'delete-teams', 'read-teams-hidden', 'update-teams-hidden', 'update-teams-membership-own'];
+        $r_admin->syncPermissions($r_admin_perms);
+
+        $r_officer_ii_perms = ['read-users', 'read-users-own', 'update-users', 'update-users-own', 'create-events',
+            'read-events', 'update-events', 'update-events-own', 'delete-events', 'create-rsvps-own', 'read-rsvps',
+            'read-rsvps-own', 'update-rsvps', 'update-rsvps-own', 'delete-rsvps', 'delete-rsvps-own', 'create-payments',
+            'read-payments', 'read-payments-own', 'create-dues-packages', 'read-dues-packages', 'update-dues-packages',
+            'read-dues-transactions', 'read-dues-transactions-own', 'update-dues-transactions',
+            'create-recruiting-visits', 'read-recruiting-visits', 'read-recruiting-visits-own',
+            'update-recruiting-visits', 'update-recruiting-visits-own', 'create-attendance', 'read-attendance',
+            'read-attendance-own', 'update-attendance', 'delete-attendance', 'create-teams', 'read-teams',
+            'update-teams', 'update-teams-membership-own'];
+        $r_officer_ii->syncPermissions($r_officer_ii_perms);
+
+        $r_officer_i_perms = ['read-users', 'read-users-own', 'update-users', 'update-users-own', 'create-events',
+            'read-events', 'update-events-own', 'create-rsvps-own', 'read-rsvps', 'read-rsvps-own', 'update-rsvps-own',
+            'delete-rsvps-own', 'create-payments', 'read-payments', 'read-payments-own', 'read-dues-packages',
+            'read-dues-transactions', 'read-dues-transactions-own', 'update-dues-transactions',
+            'create-recruiting-visits', 'read-recruiting-visits-own', 'update-recruiting-visits-own',
+            'create-attendance', 'read-attendance', 'read-attendance-own', 'update-attendance', 'delete-attendance',
+            'create-teams', 'read-teams', 'update-teams', 'update-teams-membership-own'];
+        $r_officer_i->syncPermissions($r_officer_i_perms);
+
+        $r_member_perms = ['read-users-own', 'update-users-own', 'read-events', 'create-rsvps-own', 'read-rsvps-own',
+            'update-rsvps-own', 'delete-rsvps-own', 'read-payments-own', 'read-dues-packages',
+            'read-dues-transactions-own', 'read-recruiting-visits-own', 'update-recruiting-visits-own',
+            'read-attendance-own', 'read-teams', 'update-teams-membership-own'];
+        $r_member->syncPermissions($r_member_perms);
+
+        $r_nonmember_perms = ['read-users-own', 'update-users-own', 'read-events', 'create-rsvps-own', 'read-rsvps-own',
+            'update-rsvps-own', 'delete-rsvps-own', 'read-payments-own', 'read-dues-packages',
+            'read-dues-transactions-own', 'read-recruiting-visits-own', 'update-recruiting-visits-own',
+            'read-attendance-own', 'read-teams', 'update-teams-membership-own'];
+        $r_non_member->syncPermissions($r_nonmember_perms);
+
+        $r_core_perms = ['read-users-own', 'update-users-own', 'create-events', 'read-events', 'update-events-own',
+            'create-rsvps-own', 'read-rsvps-own', 'update-rsvps-own', 'delete-rsvps-own', 'read-payments-own',
+            'read-dues-packages', 'read-dues-transactions-own', 'create-recruiting-visits',
+            'read-recruiting-visits-own', 'update-recruiting-visits-own', 'create-attendance',
+            'read-attendance', 'read-attendance-own', 'create-teams', 'read-teams', 'update-teams-membership-own'];
+        $r_core->syncPermissions($r_core_perms);
     }
 }
