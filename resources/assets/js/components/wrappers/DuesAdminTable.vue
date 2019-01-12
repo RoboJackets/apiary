6a60fe718e63609947a0634ef839e7945e6040a7
Daniel Hsing <hsing.daniel@gmail.com>
2019-01-12 17:42:27 -0500

    feat: add teams column to DuesAdminTable
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- resources/assets/js/components/wrappers/DuesAdminTable.vue
+++ resources/assets/js/components/wrappers/DuesAdminTable.vue
@@ -1,6 +1,6 @@
 <template>
   <datatable id="dues-admin-table"
-    data-url="/api/v1/dues/transactions?include=user,payment"
+    data-url="/api/v1/dues/transactions?include=user,payment,user.teams"
     data-path="dues_transactions"
     :columns="tableConfig">
   </datatable>
@@ -14,6 +14,15 @@ export default {
         { title: 'ID', data: 'id' },
         { title: 'Timestamp', data: 'created_at.date' },
         { title: 'Name', data: 'user.name' },
+        { title: 'Teams', data: function(row) {
+            const teams = row.user.teams.map(function(team) {
+              return team.name;
+            });
+            if (teams.length === 0) {
+              return "N/A";
+            }
+            return teams.join(", ");
+          }},
         { title: 'Dues Package', data: 'package.name' },
         { title: 'Status', data: 'status', className: 'text-capitalize' },
         { title: 'Amount Paid', data: 'payment[0].amount' },
