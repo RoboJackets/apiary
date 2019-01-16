835851b08a5b4b66d3170a3888ed5b412dcaa5f4
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-01-16 15:23:32 -0500

    User filter tweaks

---
+++ app/Nova/Filters/UserTeam.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+
+class UserTeam extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        return $query->belongsTo(\App\Team::class, $value);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        $teams = [];
+        if ($request->user()->can('read-teams')) {
+            $teams = Team::where('attendable', 1)
+                ->when($request->user()->cant('read-teams-hidden'), function ($query) {
+                    $query->where('visible', 1);
+                })->get()
+                ->mapWithKeys(function ($item) {
+                    return [$item['name'] => $item['id']];
+                })->toArray();
+        }
+        return $teams;
+    }
+}
