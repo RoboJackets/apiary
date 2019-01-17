eb1b99e0972c3258fafe1e6b5f2d9b8976947f3d
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-01-17 16:55:53 -0500

    Attempt fix DTPS filter

--- app/Nova/Filters/DuesTransactionPaymentStatus.php
+++ app/Nova/Filters/DuesTransactionPaymentStatus.php
@@ -25,7 +25,14 @@ class DuesTransactionPaymentStatus extends BooleanFilter
     public function apply(Request $request, $query, $value)
     {
         if ($value['pending']) {
-            return $query->pending();
+            return $query->leftJoin('payments', function ($j) {
+                $j->on('payments.payable_id', '=', 'dues_transactions.id')
+                    ->where('payments.payable_type', '=', \App\DuesTransaction::class)
+                    ->where('payments.deleted_at', '=', null);
+            })
+            ->join('dues_packages', 'dues_packages.id', '=', 'dues_transactions.dues_package_id')
+            ->groupBy('dues_transactions.id', 'dues_transactions.dues_package_id', 'dues_packages.cost')
+            ->havingRaw('COALESCE(SUM(payments.amount),0.00) < dues_packages.cost');
         } else {
             return $query;
         }
