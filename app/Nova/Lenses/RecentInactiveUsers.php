1f69a355f5182d7cb014f5f532d0e5c1e2615fc8
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-06-03 22:31:31 -0500

    More stan fixes

--- app/Nova/Lenses/RecentInactiveUsers.php
+++ app/Nova/Lenses/RecentInactiveUsers.php
@@ -14,7 +14,7 @@ use Laravel\Nova\Lenses\Lens;
 use App\Nova\Filters\Attendable;
 use Laravel\Nova\Fields\MorphTo;
 use Laravel\Nova\Fields\BelongsTo;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Laravel\Nova\Http\Requests\LensRequest;
 
 class RecentInactiveUsers extends Lens
