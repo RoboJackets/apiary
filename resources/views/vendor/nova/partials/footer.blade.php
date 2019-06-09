0eeb190b7fc31c769783247c2cb3c4296545faf1
Josh Oldenburg <josh.oldenburg@me.com>
2019-06-09 19:09:45 -0400

    Make links more subtle

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="https://github.com/RoboJackets/apiary">Made with ♥ by RoboJackets</a>
+    <a class="text-primary dim no-underline" href="https://github.com/RoboJackets/apiary">Made with ♥ by RoboJackets</a>
     <span class="px-1">&middot;</span>
-    <a class="text-muted" href="/privacy">Privacy Policy</a>
+    <a class="text-primary dim no-underline" class="text-muted" href="/privacy">Privacy Policy</a>
     <span class="px-1">&middot;</span>
-    <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSelERsYq3gLmHbWvVCWha5iCU8z3r9VYC0hCN4ArLpMAiysaQ/viewform?entry.1338203640={{ request()->fullUrl()}}">Make a Wish</a>
+    <a class="text-primary dim no-underline" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSelERsYq3gLmHbWvVCWha5iCU8z3r9VYC0hCN4ArLpMAiysaQ/viewform?entry.1338203640={{ request()->fullUrl()}}">Make a Wish</a>
 </p>
