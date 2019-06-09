4d7dc399ee9eb1954d62b8987d8067bf99c3ce0b
Josh Oldenburg <josh.oldenburg@me.com>
2019-06-09 18:43:16 -0400

    Re-add nova:publish call, it will not interfere with existing views

--- post-deploy-hook.sh
+++ post-deploy-hook.sh
@@ -7,6 +7,7 @@ php artisan migrate --no-interaction
 php artisan config:cache --no-interaction
 php artisan view:clear --no-interaction
 php artisan route:clear --no-interaction
+php artisan nova:publish --no-interaction
 php artisan horizon:assets --no-interaction
 php artisan cache:clear --no-interaction
 npm ci --no-progress
