d24369e62361d40e4583650941c8803f1b92fb0a
Josh Oldenburg <josh.oldenburg@me.com>
2019-06-09 18:52:18 -0400

    Add make a wish to the Nova footer

---
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<p class="mt-8 text-center text-xs text-80">
+    <a href="https://github.com/RoboJackets/apiary">Made with ♥ by RoboJackets</a>
+    <span class="px-1">&middot;</span>
+    <a class="text-muted" href="/privacy">Privacy Policy</a>
+    <span class="px-1">&middot;</span>
+    <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSelERsYq3gLmHbWvVCWha5iCU8z3r9VYC0hCN4ArLpMAiysaQ/viewform?entry.1338203640={{ request()->fullUrl()}}">Make a Wish</a>
+</p>
