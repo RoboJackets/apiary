b0b5930c8578bc7957fcaa7eb0c1c29912a906da
Josh Oldenburg <josh.oldenburg@me.com>
2019-08-22 13:35:18 -0400

    Ignore service accounts in sync access Nova action

--- app/Nova/Actions/SyncAccess.php
+++ app/Nova/Actions/SyncAccess.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 namespace App\Nova\Actions;
 
 use App\Jobs\PushToJedi;
+use Illuminate\Support\Str;
 use Illuminate\Bus\Queueable;
 use Laravel\Nova\Actions\Action;
 use Illuminate\Support\Collection;
@@ -29,6 +30,9 @@ class SyncAccess extends Action
     public function handle(ActionFields $fields, Collection $models): void
     {
         foreach ($models as $user) {
+            if (Str::startsWith($user->uid, 'svc_') {
+                continue;
+            }
             // I tried to make this class ShouldQueue so Nova would handle queueing
             // but was getting an exception. I think it's fine to run synchronously...?
             PushToJedi::dispatchNow($user);
