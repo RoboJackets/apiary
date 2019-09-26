9f6a233891782827026cdc350f9d1775d1ae7b1e
Josh Oldenburg <josh.oldenburg@me.com>
2019-09-25 22:56:31 -0400

    Fix #834

---
+++ app/Nova/Filters/UserAccessActive.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+// phpcs:disable Generic.CodeAnalysis.UnusedFunctionParameter,SlevomatCodingStandard.Functions.UnusedParameter,SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+
+namespace App\Nova\Filters;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Filters\Filter;
+use Illuminate\Database\Eloquent\Builder;
+
+class UserAccessActive extends Filter
+{
+    /**
+     * The displayable name of the action.
+     *
+     * @var string
+     */
+    public $name = 'Access Active';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @param \Illuminate\Database\Eloquent\Builder  $query
+     * @param string  $value
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value): Builder
+    {
+        return 'yes' === $value ? $query->accessActive() : $query->accessInactive();
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param \Illuminate\Http\Request  $request
+     *
+     * @return array<string,string>
+     */
+    public function options(Request $request): array
+    {
+        return [
+            'Yes' => 'yes',
+            'No' => 'no',
+        ];
+    }
+}
