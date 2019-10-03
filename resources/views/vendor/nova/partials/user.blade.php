3470fbe2789442699e71b3dcf210d14d1cc0ee42
Josh Oldenburg <josh.oldenburg@me.com>
2019-10-03 13:32:55 -0400

    Use config to get Nova path prefix

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -7,7 +7,7 @@
 <dropdown-menu slot="menu" width="100" direction="rtl">
     <ul class="list-reset">
         <li>
-            <a href="/nova/resources/users/{{ auth()->user()->id }}" class="block no-underline text-90 hover:bg-30 p-3">
+            <a href="{{ config('nova.path') }}/resources/users/{{ auth()->user()->id }}" class="block no-underline text-90 hover:bg-30 p-3">
                 Profile
             </a>
             <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
