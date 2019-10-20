02a04bfc25380a0e5dcc27f4b66a53e9881002b0
Kristaps Berzinch <kristapsberzinch@gmail.com>
2019-10-19 22:04:40 -0400

    Don't require authn_method field for authorization check

--- app/Http/Middleware/CASAuthenticate.php
+++ app/Http/Middleware/CASAuthenticate.php
@@ -71,7 +71,7 @@ class CASAuthenticate
                     exit;
                 }
 
-                $attrs = ['gtGTID', 'email_primary', 'givenName', 'sn', 'authn_method'];
+                $attrs = ['gtGTID', 'email_primary', 'givenName', 'sn'];
                 // Attributes that will be split by commas when masquerading
                 $arrayAttrs = ['gtPersonEntitlement', 'gtAccountEntitlement'];
                 // Merge them together so we verify all attributes are present, even the array ones
