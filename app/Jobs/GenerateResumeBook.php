ee775452a4461033d2016db4e18530275d611364
Josh Oldenburg <josh.oldenburg@me.com>
2019-12-09 14:01:09 -0500

    Fix style

--- app/Jobs/GenerateResumeBook.php
+++ app/Jobs/GenerateResumeBook.php
@@ -72,6 +72,7 @@ class GenerateResumeBook implements ShouldQueue
      * @return void
      *
      * @SuppressWarnings(PHPMD.IfStatementAssignment)
+     * @suppress PhanStaticCallToNonStatic,PhanPossiblyNonClassMethodCall,PhanTypeMismatchArgument
      */
     public function handle(): void
     {
@@ -84,14 +85,11 @@ class GenerateResumeBook implements ShouldQueue
         if (null !== $this->major) {
             $majors = $users->mapWithKeys(static function (User $user): array {
                 $ous = Cache::remember('whitepages_ou_'.$user->uid, now()->addDays(1), static function () use ($user) {
-                    // @phan-ignore-next-line PhanStaticCallToNonStatic,PhanPossiblyNonClassMethodCall,PhanTypeMismatchArgument
                     return Adldap::search()
                         ->where('uid', '=', $user->uid)
                         ->select('uid', 'ou')
                         ->get()
-                        // @phan-ignore-next-line PhanPossiblyNonClassMethodCall
                         ->pluck('ou')
-                        // @phan-ignore-next-line PhanTypeMismatchArgument
                         ->pluck(0);
                 });
 
