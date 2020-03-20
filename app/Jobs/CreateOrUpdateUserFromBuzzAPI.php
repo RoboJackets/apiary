08dde9e8c4beb8ff8db534bd978f9e63b466a460
Josh Oldenburg <josh@joldenburg.me>
2020-03-20 00:05:23 -0400

    Remove gtDirGUID field

--- app/Jobs/CreateOrUpdateUserFromBuzzAPI.php
+++ app/Jobs/CreateOrUpdateUserFromBuzzAPI.php
@@ -114,7 +114,6 @@ class CreateOrUpdateUserFromBuzzAPI implements ShouldQueue
         $user->first_name = $account->givenName;
         $user->last_name = $account->sn;
         $user->primary_affiliation = $account->eduPersonPrimaryAffiliation;
-        $user->gtDirGUID = $account->gtPersonDirectoryID;
         $user->save();
         $user->syncMajorsFromAccountEntitlements($account->gtAccountEntitlement);
 
