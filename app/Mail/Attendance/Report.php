6eecd40d8bb86eade8c8a5167f6ad8da5d7c7bde
Josh Oldenburg <josh@joldenburg.me>
2020-04-03 12:13:01 -0400

    Change reply to to be RoboJackets Officers

--- app/Mail/Attendance/Report.php
+++ app/Mail/Attendance/Report.php
@@ -40,7 +40,7 @@ class Report extends Mailable
         return $this
             ->from('attendancereports@my.robojackets.org', 'RoboJackets')
             ->withSwiftMessage(static function (SimpleMimeEntity $message): void {
-                $message->getHeaders()->addTextHeader('Reply-To', 'RoboJackets <officers@robojackets.org>');
+                $message->getHeaders()->addTextHeader('Reply-To', 'RoboJackets Officers <officers@robojackets.org>');
             })->subject('RoboJackets Attendance Report Ending '.$this->export->end_time->format('n/j/Y'))
             ->markdown('mail.attendance.report');
     }
