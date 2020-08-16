2195c9eba25ff11ae4916fcac0f74afd15decf38
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-08-16 19:55:27 -0400

    Remove unneeded phpstan ignores + horizon

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,6 @@ use App\Payment;
 use App\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Horizon\Horizon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,26 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
-
-        // @phan-suppress-next-line PhanPluginAlwaysReturnFunction
-        Horizon::auth(static function (): bool {
-            // @phan-suppress-next-line PhanPossiblyUndeclaredMethod
-            if (auth()->guard('web')->user() instanceof User
-                // @phan-suppress-next-line PhanPossiblyUndeclaredMethod
-                && auth()->guard('web')->user()->can('access-horizon')
-            ) {
-                return true;
-            }
-
-            // @phan-suppress-next-line PhanPossiblyUndeclaredMethod
-            if (null === auth()->guard('web')->user()) {
-                // Theoretically, this should never happen since we're calling the CAS middleware before this.
-                abort(401, 'Authentication Required');
-            }
-
-            abort(403, 'Forbidden');
-        });
-
         User::observe(UserObserver::class);
         Payment::observe(PaymentObserver::class);
         Attendance::observe(AttendanceObserver::class);
