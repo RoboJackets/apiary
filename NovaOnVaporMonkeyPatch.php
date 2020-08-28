c5a87ca10d1a9e4d725fa1089b9f35803e6884b0
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-08-27 20:17:32 -0400

    style

--- NovaOnVaporMonkeyPatch.php
+++ NovaOnVaporMonkeyPatch.php
@@ -1,6 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Laravel\Nova\Http\Controllers;
+
+// phpcs:disable Generic.CodeAnalysis.UnusedFunctionParameter.Found
+// phpcs:disable SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
 
 function filemtime(string $path): int
 {
