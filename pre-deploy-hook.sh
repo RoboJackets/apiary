441a599d782d4f2ae95c2271e5badd7dcfe4039f
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-10-01 19:40:53 -0400

    Fix deployment scripts

--- pre-deploy-hook.sh
+++ pre-deploy-hook.sh
@@ -6,6 +6,6 @@ cd "${0%/*}"
 
 ./pause-monitoring.sh
 
-php artisan down --message="An app upgrade is in progress. Please try again in a few minutes." --retry=60
+php artisan down --message="An app upgrade is in progress. Please try again in a few minutes." --retry=60 || true
 
 git rev-parse HEAD > .last_deployment_hash
