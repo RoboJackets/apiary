eb46898192d0858cbac19f4d27d5a9390370ab4d
Josh Oldenburg <josh@joldenburg.me>
2020-10-12 17:37:22 -0400

    Fix style

--- app/Jobs/GenerateResumeBook.php
+++ app/Jobs/GenerateResumeBook.php
@@ -14,7 +14,6 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
