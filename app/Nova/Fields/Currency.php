664ea1a80190bc7838e97fd12f49fb0388c2e0f6
Ross Lindsay <ross@rosslindsay.com>
2020-10-16 00:33:28 -0400

    Override mixed style check until someone figures out what it should be so that we can test this

--- app/Nova/Fields/Currency.php
+++ app/Nova/Fields/Currency.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+// phpcs:disable SlevomatCodingStandard.TypeHints.DisallowMixedTypeHint.DisallowedMixedTypeHint
+
 namespace App\Nova\Fields;
 
 class Currency extends \Laravel\Nova\Fields\Currency
