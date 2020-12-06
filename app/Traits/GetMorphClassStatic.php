6160f17ee895c50a162f7e7ec2ab22cee3da8c97
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-12-06 16:15:04 -0500

    style

--- app/Traits/GetMorphClassStatic.php
+++ app/Traits/GetMorphClassStatic.php
@@ -7,7 +7,7 @@ namespace App\Traits;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
 /**
- * This is a copy of a function that Laravel provides dynamically
+ * This is a copy of a function that Laravel provides dynamically.
  */
 trait GetMorphClassStatic
 {
