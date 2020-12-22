37e0a637bcecc3e55f5160fecf25861172eca6a9
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-12-22 16:33:00 -0500

    Fix Square signature validation

---
+++ app/SignatureValidators/Square.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\SignatureValidators;
+
+use Exception;
+use Illuminate\Http\Request;
+use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
+use Spatie\WebhookClient\WebhookConfig;
+
+class Square implements SignatureValidator
+{
+    /**
+     * Verifies a signature on a request from GitHub
+     */
+    public function isValid(Request $request, WebhookConfig $config): bool
+    {
+        $sentSignature = $request->header('X-Square-Signature');
+        $secret = $config->signingSecret;
+        $payload = $request->getContent();
+
+        if (!is_string($sentSignature)) {
+            throw new Exception('Header is not a string, possibly missing');
+        }
+
+        if (!is_string($payload)) {
+            throw new Exception('Payload is not a string');
+        }
+
+        $calculatedSignature = base64_encode(hash_hmac('sha1', $request->url . $payload, $secret, true));
+
+        return (sha1($calculatedSignature) === sha1($sentSignature));
+    }
+}
--- config/webhook-client.php
+++ config/webhook-client.php
@@ -27,7 +27,7 @@ return [
              *
              * It should implement \Spatie\WebhookClient\SignatureValidator\SignatureValidator
              */
-            'signature_validator' => \Spatie\WebhookClient\SignatureValidator\DefaultSignatureValidator::class,
+            'signature_validator' => App\SignatureValidators\Square::class,
 
             /*
              * This class determines if the webhook call should be stored and processed.
