ba5c3f7b5383e190906fe6341df1d746d73e5335
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-12-23 17:09:48 -0500

    Fix migration name

--- database/migrations/2020_12_23_152158_add_fiscal_year_and_affiliation_restriction_to_dues_packages.php
+++ database/migrations/2020_12_23_152158_add_fiscal_year_and_affiliation_restriction_to_dues_packages.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFiscalYearRelationshipToDuesPackages extends Migration
+class AddFiscalYearAndAffiliationRestrictionToDuesPackages extends Migration
 {
     /**
      * Run the migrations.
