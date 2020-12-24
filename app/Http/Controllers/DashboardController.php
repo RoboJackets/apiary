c5b0e7b760b80ef3ea0a6300b300d48a77d3582a
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-12-24 13:18:06 -0500

    Only show card if DuesPackage::userCanPurchase returns results

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -29,7 +29,7 @@ class DashboardController extends Controller
 
         //User needs a transaction if they don't have one for an active dues package
         $needsTransaction = (0 === DuesTransaction::current()->where('user_id', $user->id)->count());
-        $needsTransaction = $needsTransaction && (DuesPackage::availableForPurchase()->count() > 0);
+        $needsTransaction = $needsTransaction && (DuesPackage::userCanPurchase($user)->count() > 0);
 
         //User needs a payment if they don't have enough payments to cover their pending dues transaction
         //Don't change this to use ->count(). It won't work - trust me.
