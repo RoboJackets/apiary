4c25026647f94e82d5ada80e8475800a20a48ba4
Kristaps Berzinch <kristapsberzinch@gmail.com>
2020-12-30 12:25:51 -0500

    Ignore transactions labeled as retreat

--- app/Models/SquareTransaction.php
+++ app/Models/SquareTransaction.php
@@ -47,6 +47,7 @@ class SquareTransaction extends Model
             ->where('amount', '<=', $transaction->package->cost + 5)
             ->where('source', 'Point of Sale')
             ->where('customer_name', $transaction->user->first_name.' '.$transaction->user->last_name)
+            ->where('description', 'not like', '%retreat%')
             ->get();
 
         if (1 === $query->count()) {
