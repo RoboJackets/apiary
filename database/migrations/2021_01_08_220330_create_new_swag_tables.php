d22197e123295b47a596aa78af1e8ca4bb8aaf95
Josh Oldenburg <josh@joldenburg.me>
2021-01-09 23:07:19 -0500

    Add initial migration

---
+++ database/migrations/2021_01_08_220330_create_new_swag_tables.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewSwagTables extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('merch', static function (Blueprint $table): void {
+            $table->id();
+            $table->string('name', 255);
+            $table->foreignId('fiscal_year_id')->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('dues_package_merch', static function (Blueprint $table): void {
+            $table->id();
+            $table->foreignId('fiscal_year_id')->constrained();
+            $table->foreignId('merch_id')->constrained();
+            $table->timestamps();
+        });
+
+        Schema::create('dues_transaction_merch', static function (Blueprint $table): void {
+            $table->id();
+            $table->foreignId('fiscal_year_id')->constrained();
+            $table->foreignId('merch_id')->constrained();
+            $table->timestamp('provided_at');
+            $table->unsignedInteger('provided_by');
+            $table->timestamps();
+
+            $table->foreign('provided_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dues_transaction_merch');
+        Schema::dropIfExists('dues_package_merch');
+        Schema::dropIfExists('merch');
+    }
+}
