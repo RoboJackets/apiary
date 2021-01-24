b67d90398077d46f9ba54da92b14fd59de9d97ab
Kristaps Berzinch <kristapsberzinch@gmail.com>
2021-01-24 17:22:27 -0500

    whitespace

--- app/Jobs/ProcessSquareWebhook.php
+++ app/Jobs/ProcessSquareWebhook.php
@@ -41,6 +41,7 @@ class ProcessSquareWebhook extends ProcessWebhookJob
 
         if ('COMPLETED' !== $details['status']) {
             Log::warning('Payment for Order ID '.$details['order_id'].'was pushed as '.$details['status']);
+
             return;
         }
 
