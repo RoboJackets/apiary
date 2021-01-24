5f88f116e88130cda7710086a5fe58f4ec144790
Josh Oldenburg <josh@joldenburg.me>
2021-01-23 19:38:08 -0500

    Fix typo

--- database/migrations/2021_01_23_191816_remove_swag_fields_from_dues_packages.php
+++ database/migrations/2021_01_23_191816_remove_swag_fields_from_dues_packages.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RemoveSwagFieldsFromDuesPackagess extends Migration
+class RemoveSwagFieldsFromDuesPackages extends Migration
 {
     /**
      * Run the migrations.
