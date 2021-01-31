1627a29b15f6d159da15c1e3bf52d233e7f26203
Kristaps Berzinch <kristapsberzinch@gmail.com>
2021-01-31 15:39:57 -0500

    Allow non-members to read merchandise Fixes #1601

---
+++ database/migrations/2021_01_31_153259_give_non_members_access_to_read_merchandise.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class GiveNonMembersAccessToReadMerchandise extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Reset cached roles and permissions
+        app()['cache']->forget('spatie.permission.cache');
+
+        $memberRole = Role::firstOrCreate(['name' => 'non-member']);
+        $memberRole->givePermissionTo('read-merchandise');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // nope
+    }
+}
