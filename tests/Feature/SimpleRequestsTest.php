22be946a29c60f2c59b60bff54b1186c75b53bce
Josh Oldenburg <josh@joldenburg.me>
2021-04-28 20:37:12 -0400

    Get first functional authenticated web tests

--- tests/Feature/SimpleRequestsTest.php
+++ tests/Feature/SimpleRequestsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,24 +12,39 @@ class SimpleRequestsTest extends TestCase
     use RefreshDatabase;
 
     /**
-	 * Test simple, non-CAS requests work.
+     * Test simple, non-CAS requests work.
      */
     public function test_unauthenticatedRequests(): void
     {
         $response = $this->get('/privacy');
         $response->assertStatus(200);
 
-		$response = $this->get('/attendance/kiosk');
+        $response = $this->get('/attendance/kiosk');
         $response->assertStatus(200);
     }
 
     /**
-	 * Test that the home page works.
+     * Test that the home page works.
      */
     public function test_home(): void
     {
-        $response = $this->get('/');
-        print_r($response);
+        $user = new User();
+        $user->create_reason = 'phpunit';
+        $user->is_service_account = false;
+        $user->uid = 'apiarytesting4';
+        $user->gtid = 901234567;
+        $user->gt_email = 'robojackets-it@lists.gatech.edu';
+        $user->first_name = 'Apiary';
+        $user->last_name = 'PHPUnit';
+        $user->primary_affiliation = 'student';
+        $user->has_ever_logged_in = true;
+        $user->save();
+
+        $response = $this->actingAs($user, 'web')->get('/');
+        $response->assertStatus(200);
+        $response->dump();
+
+        $response = $this->actingAs($user, 'web')->get('/');
         $response->assertStatus(200);
     }
 }
