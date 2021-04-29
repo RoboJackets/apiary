f6255f8550470ca7c38b0986e2ef9eb7d5761d55
Josh Oldenburg <josh@joldenburg.me>
2021-04-28 20:48:22 -0400

    Fix style

--- tests/Feature/SimpleRequestsTest.php
+++ tests/Feature/SimpleRequestsTest.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class SimpleRequestsTest extends TestCase
