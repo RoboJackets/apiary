eb2e09503e36e74b56747df2dbda98e05c7aef5f
Evan Strat <evan10s@users.noreply.github.com>
2021-05-26 19:38:58 -0400

    middleware tweaks to make passport happy

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,7 @@ class Kernel extends HttpKernel
      * @var array<string,string>
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\CASCheck::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
