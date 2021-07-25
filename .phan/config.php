fa30a9c72e17e27b9c1f8144596b4c5f23bfa4cd
Kristaps Berzinch <kristapsberzinch@gmail.com>
2021-07-25 16:52:42 -0400

    Ignore phan false positive

--- .phan/config.php
+++ .phan/config.php
@@ -35,6 +35,7 @@ return [
         'PhanPossiblyFalseTypeArgumentInternal',
         'PhanPossiblyNonClassMethodCall',
         'PhanPossiblyNullTypeArgument',
+        'PhanPossiblyUndeclaredMethod',
         'PhanReadOnlyPHPDocProperty',
         'PhanReadOnlyProtectedProperty',
         'PhanUndeclaredFunctionInCallable',
