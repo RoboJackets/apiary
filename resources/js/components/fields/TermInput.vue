19afb02b7cca7ec21552cffc5698389b5f9c6c1e
Sarah Storer <sastorer15@gmail.com>
2021-08-02 21:20:55 -0700

    Trying to select by id again

--- resources/js/components/fields/TermInput.vue
+++ resources/js/components/fields/TermInput.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="form-inline" :class="{ 'is-invalid': isError }">
-    <select v-model="semester" class="custom-select" :class="{ 'is-invalid': isError && semester.length !== 2 }">
+    <select v-model="semester" id="semesterSelect" class="custom-select" :class="{ 'is-invalid': isError && semester.length !== 2 }">
       <option value="" style="display:none;">Semester</option>
       <option value="08">Fall</option>
       <option value="02">Spring</option>
       <option value="05">Summer</option>
     </select>
     <input v-if="semester.length === 2"
-      v-model="year" class="form-control" :class="{ 'is-invalid': isError }" maxlength="4" size="6" type="number" min="2000" max="3000" placeholder="Year">
+      v-model="year" id="yearSelect" class="form-control" :class="{ 'is-invalid': isError }" maxlength="4" size="6" type="number" min="2000" max="3000" placeholder="Year">
     <button type="radio" class="btn btn-secondary float-right" @change="change">Current Semester</button>
   </div>
 </template>
@@ -44,8 +44,8 @@ export default {
       },
       set: function(newSemester) {
         var term = this.year + '' + newSemester;
-
         this.$emit('input', term);
+        
         if (this.term && this.term.length === 6) {
           this.$emit('touch', term)
         }
@@ -76,13 +76,18 @@ export default {
       var d = new Date();
       var month = d.getMonth();
       var year = d.getFullYear();
+      console.log("change was called");
       if (month <= 5) {
+        document.getElementById('semesterSelect').value = '02'
         this.semester.set("02");
       } else if (month <= 9) {
+        document.getElementById('semesterSelect').value = '05'
         this.semester.set("05");
       } else if (month <= 12) {
+        document.getElementById('semesterSelect').value = '08'
         this.semester.set("08");
       }
+      document.getElementById('yearSelect').value = year.toString();
       this.year.set(year.toString());
     },
   },
