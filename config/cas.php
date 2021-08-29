9f4551a1d7048d4aae3ec33a964100fccf4f1965
Kristaps Berzinch <kristapsberzinch@gmail.com>
2021-08-29 14:06:31 -0400

    Add support for configuring CAS version

--- config/cas.php
+++ config/cas.php
@@ -136,6 +136,15 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | CAS will support version 1.0, 2.0, 3.0 of the protocol.  It is recommended
+    | to use version 2.0, 3.0, or SAML 1.1.  If you enable SAML, then that
+    | will override this configuration.
+    |--------------------------------------------------------------------------
+    */
+    'cas_version'         => env('CAS_VERSION', "2.0"),
+    
+    /*
+    |--------------------------------------------------------------------------
     | Enable PHPCas Debug Mode
     | Options are:
     | 1) true (defaults logfile creation to /tmp/phpCAS.log)
