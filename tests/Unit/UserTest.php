33d1dd55df24fdb23b2ed741aee7974063903dae
Josh Oldenburg <josh@joldenburg.me>
2021-10-09 00:11:11 -0400

    Add test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\DuesPackage;
+use App\Models\DuesTransaction;
+use App\Models\Payment;
+use App\Models\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * Test the is_student attribute.
+     *
+     * @return void
+     */
+    public function test_isStudent()
+    {
+        $user = $this->getTestUser(['member']);
+
+        $user->primary_affiliation = 'member';
+        $this->assertFalse($user->is_student);
+        $user->primary_affiliation = 'faculty';
+        $this->assertFalse($user->is_student);
+        $user->primary_affiliation = 'staff';
+        $this->assertFalse($user->is_student);
+        $user->primary_affiliation = 'employee';
+        $this->assertFalse($user->is_student);
+        $user->primary_affiliation = 'affiliate';
+        $this->assertFalse($user->is_student);
+
+        $user->primary_affiliation = 'student';
+        $this->assert($user->is_student);
+
+        // Create dues package / transaction / payment
+        $package = DuesPackage::factory()->make();
+        $package->effective_end = now()->addMonths(10);
+        $package->restricted_to_students = true;
+        $package->save();
+        $transaction = new DuesTransaction();
+        $transaction->user_id = $user->id;
+        $transaction->dues_package_id = $package->id;
+        $transaction->save();
+        $payment = new Payment();
+        $payment->payable_id = $transaction->id;
+        $payment->payable_type = DuesTransaction::getMorphClassStatic();
+        $payment->method = 'cash';
+        $payment->amount = $package->cost;
+        // @phan-suppress-next-line PhanTypeMismatchPropertyProbablyReal
+        $payment->processing_fee = 0;
+        $payment->save();
+
+        // They paid a student package
+        $this->assert($user->is_student);
+
+        $package->restricted_to_students = false;
+        $package->save();
+
+        // They paid a non-student package
+        $this->assertFalse($user->is_student);
+    }
+}
