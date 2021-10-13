9217ffccbda879216d56d8a0450b1fb18b5c3889
Kristaps Berzinch <kristapsberzinch@gmail.com>
2021-10-13 14:56:13 -0400

    Remove unique constraint from receipt_number column

---
+++ database/migrations/2021_10_13_145044_remove_unique_constraint_from_receipt_number.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveUniqueConstraintFromReceiptNumber extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('payments', static function (Blueprint $table): void {
+            $table->dropUnique('payments_receipt_number_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('payments', static function (Blueprint $table): void {
+            $table->unique('receipt_number');
+        });
+    }
+}
