f75b0e71084c98a77215ee2e7c828e3eaa993052
Josh Oldenburg <josh@joldenburg.me>
2021-12-14 20:59:23 -0500

    Revert "Fix attendance export action permissions"
    This reverts commit 16dfe489e8987f0cc7bb2e9d81e8c284ad1bd39b.

--- app/Nova/Attendance.php
+++ app/Nova/Attendance.php
@@ -185,9 +185,9 @@ class Attendance extends Resource
     {
         return [
             (new ExportAttendance())->canSee(static function (Request $request): bool {
-                return $request->user()->can('read-attendance');
+                return $request->user()->can('read-attendance') && $request->user()->can('read-users-gtid');
             })->canRun(static function (Request $request): bool {
-                return $request->user()->can('read-attendance');
+                return $request->user()->can('read-attendance') && $request->user()->can('read-users-gtid');
             })->confirmButtonText('Export Attendance'),
         ];
     }
