d0fffa969e456efc111949255f090014bca7cd6c
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-01-14 17:33:36 -0500

    Whitespace to appease StyleCI

--- app/Http/Controllers/TravelAssignmentController.php
+++ app/Http/Controllers/TravelAssignmentController.php
@@ -70,24 +70,37 @@ class TravelAssignmentController extends Controller
     {
         return config('docusign.travel_authority_request.powerform_url').'&'.http_build_query(
             [
-                config('docusign.travel_authority_request.fields.state_contract_airline')
-                    => $assignment->travel->tar_transportation_mode['state_contract_airline'] ? 'x' : 0,
-                config('docusign.travel_authority_request.fields.non_contract_airline')
-                    => $assignment->travel->tar_transportation_mode['non_contract_airline'] ? 'x' : 0,
-                config('docusign.travel_authority_request.fields.personal_automobile')
-                    => $assignment->travel->tar_transportation_mode['personal_automobile'] ? 'x' : 0,
-                config('docusign.travel_authority_request.fields.rental_vehicle')
-                    => $assignment->travel->tar_transportation_mode['rental_vehicle'] ? 'x' : 0,
-                config('docusign.travel_authority_request.fields.other')
-                    => $assignment->travel->tar_transportation_mode['other'] ? 'x' : 0,
+                config(
+                    'docusign.travel_authority_request.fields.state_contract_airline'
+                ) => $assignment->travel->tar_transportation_mode['state_contract_airline'] ? 'x' : 0,
+
+                config(
+                    'docusign.travel_authority_request.fields.non_contract_airline'
+                ) => $assignment->travel->tar_transportation_mode['non_contract_airline'] ? 'x' : 0,
+
+                config(
+                    'docusign.travel_authority_request.fields.personal_automobile'
+                ) => $assignment->travel->tar_transportation_mode['personal_automobile'] ? 'x' : 0,
+
+                config(
+                    'docusign.travel_authority_request.fields.rental_vehicle'
+                ) => $assignment->travel->tar_transportation_mode['rental_vehicle'] ? 'x' : 0,
+
+                config(
+                    'docusign.travel_authority_request.fields.other'
+                ) => $assignment->travel->tar_transportation_mode['other'] ? 'x' : 0,
+
                 config('docusign.travel_authority_request.fields.itinerary') => $assignment->travel->tar_itinerary,
                 config('docusign.travel_authority_request.fields.purpose') => $assignment->travel->tar_purpose,
                 config('docusign.travel_authority_request.fields.airfare_cost') => $assignment->travel->tar_airfare,
                 config('docusign.travel_authority_request.fields.other_cost') => $assignment->travel->tar_other_trans,
                 config('docusign.travel_authority_request.fields.mileage_cost') => $assignment->travel->tar_mileage,
                 config('docusign.travel_authority_request.fields.lodging_cost') => $assignment->travel->tar_lodging,
-                config('docusign.travel_authority_request.fields.registration_cost')
-                    => $assignment->travel->tar_registration,
+
+                config(
+                    'docusign.travel_authority_request.fields.registration_cost'
+                ) => $assignment->travel->tar_registration,
+
                 config('docusign.travel_authority_request.fields.total_cost') => (
                     $assignment->travel->tar_airfare +
                     $assignment->travel->tar_other_trans +
@@ -95,18 +108,30 @@ class TravelAssignmentController extends Controller
                     $assignment->travel->tar_lodging +
                     $assignment->travel->tar_registration
                 ),
-                config('docusign.travel_authority_request.fields.departure_date')
-                    => $assignment->travel->departure_date->toDateString(),
-                config('docusign.travel_authority_request.fields.return_date')
-                    => $assignment->travel->return_date->toDateString(),
-                config('docusign.travel_authority_request.traveler_name').'_UserName'
-                    => $assignment->user->name,
-                config('docusign.travel_authority_request.traveler_name').'_Email'
-                    => $assignment->user->uid.'@gatech.edu',
-                config('docusign.travel_authority_request.treasurer_name').'_UserName'
-                    => config('docusign.treasurer_name'),
-                config('docusign.travel_authority_request.treasurer_name').'_Email'
-                    => config('docusign.treasurer_account'),
+
+                config(
+                    'docusign.travel_authority_request.fields.departure_date'
+                ) => $assignment->travel->departure_date->toDateString(),
+
+                config(
+                    'docusign.travel_authority_request.fields.return_date'
+                ) => $assignment->travel->return_date->toDateString(),
+
+                config(
+                    'docusign.travel_authority_request.traveler_name'
+                ).'_UserName' => $assignment->user->name,
+
+                config(
+                    'docusign.travel_authority_request.traveler_name'
+                ).'_Email' => $assignment->user->uid.'@gatech.edu',
+
+                config(
+                    'docusign.travel_authority_request.treasurer_name'
+                ).'_UserName' => config('docusign.treasurer_name'),
+
+                config(
+                    'docusign.travel_authority_request.treasurer_name'
+                ).'_Email' => config('docusign.treasurer_account'),
             ]
         );
     }
