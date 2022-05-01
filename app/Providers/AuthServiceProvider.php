9fc4f5747747efe830b58a266507183214d25493
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-05-01 14:38:16 -0400

    Set Passport API token cookie name explicitly

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -94,5 +94,6 @@ class AuthServiceProvider extends ServiceProvider
         Passport::tokensExpireIn(now()->addDay());
         Passport::refreshTokensExpireIn(now()->addMonth());
         Passport::personalAccessTokensExpireIn(now()->addYear());
+        Passport::cookie('__Host-apiary_token');
     }
 }
