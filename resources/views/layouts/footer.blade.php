f3b4dcde6f7da5d72c802c9957fe8a90f144315c
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-05-14 14:01:38 -0400

    Add null check for footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,10 +5,10 @@
         <p class="text-muted text-right"><a class="text-muted" href="https://github.com/RoboJackets/apiary">Made with ♥ by RoboJackets</a> • <a class="text-muted" href="/privacy">Privacy Policy</a></p>
       </div>
       <div>
-        <p>
+        <p class="text-muted">
         <a class="text-muted text-left" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSelERsYq3gLmHbWvVCWha5iCU8z3r9VYC0hCN4ArLpMAiysaQ/viewform?entry.1338203640={{ $request->fullUrl()}}">Make a Wish</a>
 @if(\Sentry\Laravel\Integration::currentTracingSpan() !== null)
-@if(auth()->user()->hasRole('admin'))
+@if(auth()->user() && auth()->user()->hasRole('admin'))
 • <a class="text-muted" href="https://sentry.io/organizations/robojackets/performance/trace/{{ \Sentry\Laravel\Integration::currentTracingSpan()->getTraceId() }}">Trace ID: {{ \Sentry\Laravel\Integration::currentTracingSpan()->getTraceId() }}</a>
 @else
 • <a class="text-muted" href="#">Trace ID: {{ \Sentry\Laravel\Integration::currentTracingSpan()->getTraceId() }}</a>
