596f713c4d8eb138599e3a6811b479495c227b50
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-05-15 16:47:59 -0400

    Use Laravel global helper for checking URI

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -73,8 +73,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
      */
     protected function cards(): array
     {
-        if ('/nova-api/dashboards/main' !== $_SERVER['REQUEST_URI'] &&
-            ! Str::startsWith($_SERVER['REQUEST_URI'], '/nova-api/metrics/')) {
+        if (! request()->is('nova-api/dashboards/main') &&
+            ! request()->is('nova-api/metrics/*')) {
             return [];
         }
 
