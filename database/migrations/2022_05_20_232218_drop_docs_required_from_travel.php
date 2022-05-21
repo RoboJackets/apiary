d98572b007fcfd24b17a2b97e2bcb1dbbd08a2e3
Josh Oldenburg <josh@joldenburg.me>
2022-05-21 09:59:34 -0400

    Fix spelling

--- database/migrations/2022_05_20_232218_drop_docs_required_from_travel.php
+++ database/migrations/2022_05_20_232218_drop_docs_required_from_travel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DropDocsRequriedFromTravel extends Migration
+class DropDocsRequiredFromTravel extends Migration
 {
     /**
      * Run the migrations.
