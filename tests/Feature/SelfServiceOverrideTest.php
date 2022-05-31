8509e9d5d45139218bfbd445502c05236d326f3e
Evan Strat <evan10s@users.noreply.github.com>
2022-05-30 20:15:25 -0400

    Fix StyleCI errors

--- tests/Feature/SelfServiceOverrideTest.php
+++ tests/Feature/SelfServiceOverrideTest.php
@@ -17,21 +17,22 @@ use Tests\TestCase;
 
 class SelfServiceOverrideTest extends TestCase
 {
-    public function create_dues_package() {
+    public function create_dues_package()
+    {
         $fy = FiscalYear::create([
-            "ending_year" => Carbon::now()->year,
+            'ending_year' => Carbon::now()->year,
         ]);
 
         $pkg = DuesPackage::create([
-            "fiscal_year_id" => $fy->id,
-            "effective_start" => Carbon::now()->subMonth(),
-            "effective_end" => Carbon::now()->addMonth(),
-            "access_start" => Carbon::now()->subMonth(),
-            "access_end" => Carbon::now()->addMonth(),
-            "cost" => 10,
-            "available_for_purchase" => true,
-            "name" => "Test dues package",
-            "restricted_to_students" => false,
+            'fiscal_year_id' => $fy->id,
+            'effective_start' => Carbon::now()->subMonth(),
+            'effective_end' => Carbon::now()->addMonth(),
+            'access_start' => Carbon::now()->subMonth(),
+            'access_end' => Carbon::now()->addMonth(),
+            'cost' => 10,
+            'available_for_purchase' => true,
+            'name' => 'Test dues package',
+            'restricted_to_students' => false,
         ]);
 
         return $pkg;
@@ -71,39 +72,39 @@ class SelfServiceOverrideTest extends TestCase
         $dues_package = $this->create_dues_package();
 
         $this->assertFalse($user->self_service_override_eligibility->eligible, $user->self_service_override_eligibility);
-        $this->assertContains("None",
+        $this->assertContains('None',
             $user->self_service_override_eligibility->getUnmetConditions(),
             $user->self_service_override_eligibility);
-        $this->assertContains("Attend a team meeting",
+        $this->assertContains('Attend a team meeting',
             $user->self_service_override_eligibility->getRemainingTasks(),
             $user->self_service_override_eligibility);
-        $this->assertContains("Sign the membership agreement",
+        $this->assertContains('Sign the membership agreement',
             $user->self_service_override_eligibility->getRemainingTasks(),
             $user->self_service_override_eligibility);
 
         $this->seed(TeamsSeeder::class);
         $team = Team::first();
         Attendance::create([
-            "attendable_type" => "team",
-            "attendable_id" => $team->id,
-            "gtid" => $user->gtid,
+            'attendable_type' => 'team',
+            'attendable_id' => $team->id,
+            'gtid' => $user->gtid,
         ]);
 
         $this->assertFalse($user->self_service_override_eligibility->eligible, $user->self_service_override_eligibility);
-        $this->assertContains("None",
+        $this->assertContains('None',
             $user->self_service_override_eligibility->getUnmetConditions(),
             $user->self_service_override_eligibility);
-        $this->assertContains("Sign the membership agreement",
+        $this->assertContains('Sign the membership agreement',
             $user->self_service_override_eligibility->getRemainingTasks(),
             $user->self_service_override_eligibility);
 
         $this->create_membership_agreement_signature($user, true);
 
         $this->assertTrue($user->self_service_override_eligibility->eligible, $user->self_service_override_eligibility);
-        $this->assertContains("None",
+        $this->assertContains('None',
             $user->self_service_override_eligibility->getUnmetConditions(),
             $user->self_service_override_eligibility);
-        $this->assertContains("None",
+        $this->assertContains('None',
             $user->self_service_override_eligibility->getRemainingTasks(),
             $user->self_service_override_eligibility);
     }
