c93de7c1e2095cc1b5b8eef4bd2b36558a512116
Evan Strat <evan10s@users.noreply.github.com>
2022-06-01 23:19:47 -0400

    Ignore a couple problematic lines for CodeSniffer

--- app/Models/User.php
+++ app/Models/User.php
@@ -964,9 +964,11 @@ class User extends Authenticatable
     {
         $ineligibleReason = 'Unable to provide a self-service override at this time';
 
+        // phpcs:ignore
         $INELIGIBLE_NO_FUTURE_DUES_PKG = 'Self-service access overrides are currently unavailable because there are '.
             'no dues packages with future access end dates';
         $INELIGIBLE_REQ_CONDS = 'Account and system conditions for self-service override not met';
+        // phpcs:ignore
         $INELIGIBLE_REQ_TASKS = 'You have outstanding required tasks that must be completed before receiving your '.
             'self-service override';
 
