de15a3e06ac15a146bff351ece2a71f39d76c83c
Evan Strat <evan10s@users.noreply.github.com>
2022-06-01 23:14:27 -0400

    Fix StyleCI errors

--- app/Models/User.php
+++ app/Models/User.php
@@ -964,10 +964,10 @@ class User extends Authenticatable
     {
         $ineligibleReason = 'Unable to provide a self-service override at this time';
 
-        $INELIGIBLE_NO_FUTURE_DUES_PKG = 'Self-service access overrides are currently unavailable because there are ' .
+        $INELIGIBLE_NO_FUTURE_DUES_PKG = 'Self-service access overrides are currently unavailable because there are '.
             'no dues packages with future access end dates';
         $INELIGIBLE_REQ_CONDS = 'Account and system conditions for self-service override not met';
-        $INELIGIBLE_REQ_TASKS = 'You have outstanding required tasks that must be completed before receiving your ' .
+        $INELIGIBLE_REQ_TASKS = 'You have outstanding required tasks that must be completed before receiving your '.
             'self-service override';
 
         $now = CarbonImmutable::now();
