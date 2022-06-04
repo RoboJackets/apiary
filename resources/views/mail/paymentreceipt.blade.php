fa43cd274d8c84e408fb1bd660d86b8b2309d88d
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-06-04 15:03:07 -0400

    Add period after receipt link
    Co-authored-by: Evan Strat <5790137+evan10s@users.noreply.github.com>

--- resources/views/mail/paymentreceipt.blade.php
+++ resources/views/mail/paymentreceipt.blade.php
@@ -3,7 +3,7 @@ Hi {{ $payment->payable->user->preferred_first_name }},
 @if($payment->method === "square")
 We received your online payment of ${{ number_format($payment->amount, 2) }} for {{ $payable_name }}.
 
-You can view more details about your payment at {{ $payment->receipt_url }}
+You can view more details about your payment at {{ $payment->receipt_url }}.
 @else
 {{ $payment->user->name }} accepted your ${{ number_format($payment->amount, 2) }} {{ $payment->method }} payment for {{ $payable_name }}.
 @endif
