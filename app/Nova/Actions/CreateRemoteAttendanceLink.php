79b30fa72f2aa539a6a465e9afb9883e73532f17
Josh Oldenburg <josh@joldenburg.me>
2022-06-11 11:29:21 -0400

    Do not require URL validation for RAL creation as it requries a scheme
    The regex will ensure it is valid. Fixes #2528.

--- app/Nova/Actions/CreateRemoteAttendanceLink.php
+++ app/Nova/Actions/CreateRemoteAttendanceLink.php
@@ -91,7 +91,7 @@ class CreateRemoteAttendanceLink extends Action
         return [
             Text::make('Redirect URL')
                 ->required(false)
-                ->rules('nullable', 'url', 'regex:'.RemoteAttendanceLink::$redirectRegex, 'max:1023')
+                ->rules('nullable', 'regex:'.RemoteAttendanceLink::$redirectRegex, 'max:1023')
                 ->help('If you put a link to a Google Meet, BlueJeans, Zoom, or Microsoft Teams meeting here, '.
                     'everyone who clicks the attendance link will be redirected to that meeting after their '.
                     'attendance is recorded. If you add a redirect URL, do not share that URL directly. Only Google '.
