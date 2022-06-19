871e4901c8facf3c27d2580d0439e1681fc152ee
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-06-18 21:16:22 -0400

    Tweak whitespace for emails

--- resources/views/mail/travel/assignmentreminder.blade.php
+++ resources/views/mail/travel/assignmentreminder.blade.php
@@ -2,6 +2,7 @@ Hi {{ $assignment->user->preferred_first_name }},
 
 @if(!$assignment->tar_received && $assignment->travel->tar_required)
 You still need to submit a Travel Authority Request for {{ $assignment->travel->name }}. Please visit {{ route('docusign.travel') }} to fill out and sign it.
+
 @endif
 @if(!$assignment->is_paid)
 You{{ (!$assignment->tar_received && $assignment->travel->tar_required) ? ' also': '' }} still need to make a ${{ intval($assignment->travel->fee_amount) }} payment{{ (!$assignment->tar_received && $assignment->travel->tar_required) ? '' : ' for '.$assignment->travel->name }}.
@@ -11,8 +12,8 @@ You can pay online with a credit or debit card at {{ route('pay.travel') }}. Not
 If you would prefer to pay by cash or check, make arrangements with {{ $assignment->travel->primaryContact->full_name }}.
 
 Write checks to Georgia Tech, with RoboJackets on the memo line. Don't forget to sign it!
-@endif
 
+@endif
 For more information, visit {{ route('travel.index') }}. If you have any questions, please contact {{ $assignment->travel->primaryContact->full_name }}.
 
 ----
