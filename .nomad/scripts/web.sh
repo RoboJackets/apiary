653a4686e1e6843939b20dd8bca4d2a896d650b9
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-06-22 19:39:44 -0400

    Use exec for long-running containers Closes #2533

--- .nomad/scripts/web.sh
+++ .nomad/scripts/web.sh
@@ -9,4 +9,4 @@ php artisan config:cache --no-interaction --verbose
 php artisan view:cache --no-interaction --verbose
 php artisan event:cache --no-interaction --verbose
 php artisan route:cache --no-interaction --verbose
-php-fpm8.1 --force-stderr --nodaemonize --fpm-config /etc/php/8.1/fpm/php-fpm.conf
+exec php-fpm8.1 --force-stderr --nodaemonize --fpm-config /etc/php/8.1/fpm/php-fpm.conf
