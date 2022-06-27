e360db8b17ca38fd0659c387d8f8a0651d43e7f5
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-06-27 18:57:30 -0400

    Add instructions for downloading DocuSign forms to completion email

--- resources/views/mail/travel/allassignmentscomplete.blade.php
+++ resources/views/mail/travel/allassignmentscomplete.blade.php
@@ -1,6 +1,10 @@
 Hi {{ $travel->primaryContact->preferred_first_name }},
 
 All travelers have paid the travel fee{{ $travel->tar_required ? ' and submitted Travel Authority Requests' : '' }} for {{ $travel->name }}. Contact the treasurer to book travel.
+@if($travel->tar_required)
+
+You can download all submitted forms from {{ config('app.name') }} at {{ route('nova.pages.detail', ['resource' => 'travel', 'resourceId' => $travel->id]) }}, under the action menu (three dots in top right).
+@endif
 
 Bon voyage!
 
