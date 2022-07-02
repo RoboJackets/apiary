107a50f2f414c6e7b728fcc07b31a1d5481fe606
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-07-02 12:51:17 -0400

    Various improvements to Nginx configuration

--- resources/views/favicon.blade.php
+++ resources/views/favicon.blade.php
@@ -1,5 +1,5 @@
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-<link rel="mask-icon" href="{{ asset('safari-pinned-tab.svg') }}" color="#eeb211">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon-16x16.png') }}">
+<link rel="mask-icon" href="{{ asset('img/safari-pinned-tab.svg') }}" color="#eeb211">
 <meta name="theme-color" content="#ffffff">
