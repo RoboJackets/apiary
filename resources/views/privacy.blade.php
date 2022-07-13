cf511d790ec28102546d917f05ff31d7e4b2acbd
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-07-13 18:51:50 -0400

    Add DocuSign to privacy policy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -66,6 +66,7 @@
                 <li>Information regarding an individual payment, including your Georgia Tech email address and phone number, will be shared with Square, RoboJackets' designated payments processor, for the purposes of completing a credit/debit card transaction. You can opt-out of this sharing by using another payment method.
                 <li>Your Georgia Tech username and information about your device, as reported by your web browser or RoboJackets Android application, may be shared with Sentry, a product of Functional Software, Inc, to help us troubleshoot issues you may experience while using RoboJackets services.</li>
                 <li>Your name and Georgia Tech email address may be shared with Postmark, a product of ActiveCampaign, LLC, to deliver emails, including, but not limited to, reminders and payment receipts.</li>
+                <li>Your name, Georgia Tech email address, home department, and phone number may be shared with DocuSign, a product of DocuSign, Inc, to enable you to sign documents electronically.</li>
                 <li>Your IP address may be shared with ipstack, a product of APILayer, to determine your approximate geographical location when signing documents.</li>
                 <li>All other information will be released only to RoboJackets officers and officer-designated RoboJackets members in the course of conducting business.</li>
             </ul>
