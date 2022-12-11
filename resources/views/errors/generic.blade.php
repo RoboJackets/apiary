c3ee61e55e743bd71d661ab27fe5a153d9ee8e37
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-12-11 10:38:37 -0500

    Check for access denied error when linking GitHub Fixes #1344

--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -13,10 +13,7 @@
     <div class="row">
         <div class="col-md-12">
             Something went wrong. Please try your request again later. If you continue to receive this error,
-            please contact #it-helpdesk on <a href="https://robojackets.slack.com">Slack</a>.<br/><br>
-            <strong>If you submitted payment information before reaching this screen, try refreshing this page once.</strong> 
-            If you continue to receive this error, please contact the treasurer to verify the status of your payment.
-            You should not re-enter your card information unless instructed.
+            please contact #it-helpdesk on <a href="https://robojackets.slack.com">Slack</a>.
         </div>
     </div>
     <hr>
