ddcd438e2e52542fc04e789a1c4d8168a27f602e
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-12-11 16:27:42 -0500

    Tweak a few things so config validation passes in docker compose

--- config-validation/app.php
+++ config-validation/app.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 use AshAllenDesign\ConfigValidator\Services\Rule;
 
 return [
-    Rule::make('url')->rules([
-        'required',
-        'string',
-        'active_url',
-    ]),
+    Rule::make('url')
+        ->rules([
+            'required',
+            'string',
+        ]),
+
+    Rule::make('key')
+        ->rules([
+            'required',
+            'string',
+        ]),
 ];
