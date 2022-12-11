670a7172b7ca91f1bc85d0ce4b2686896c2ff613
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-12-11 11:48:27 -0500

    Enable safety and performance mechanisms Closes #3054

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 // @phan-file-suppress PhanPluginAlwaysReturnFunction
+// phpcs:disable Generic.CodeAnalysis.UnusedFunctionParameter.FoundInExtendedClass
+// phpcs:disable Generic.CodeAnalysis.UnusedFunctionParameter.FoundInExtendedClassAfterLastUsed
+// phpcs:disable SlevomatCodingStandard.Functions.DisallowNamedArguments.DisallowedNamedArgument
+// phpcs:disable SlevomatCodingStandard.Functions.UnusedParameter.UnusedParameter
 
 namespace App\Providers;
 
@@ -25,12 +29,21 @@ use App\Observers\MembershipAgreementTemplateObserver;
 use App\Observers\PaymentObserver;
 use App\Observers\TravelAssignmentObserver;
 use App\Observers\UserObserver;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\MasterSupervisor;
 use Laravel\Passport\Passport;
+use Sentry\EventHint;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -60,6 +73,35 @@ class AppServiceProvider extends ServiceProvider
             MasterSupervisor::determineNameUsing(static fn (): string => config('horizon.master_supervisor_name'));
         }
 
+        Model::shouldBeStrict();
+
+        if ($this->app->isProduction()) {
+            Model::handleLazyLoadingViolationUsing(static function (Model $model, string $relation): void {
+                \Sentry\captureMessage('Attemped to lazy-load '.$relation.' on '.$model::class);
+            });
+        }
+
+        DB::whenQueryingForLongerThan(100, static function (Connection $connection): void {
+            \Sentry\captureMessage('Total database query time exceeded 100ms');
+        });
+
+        DB::listen(static function (QueryExecuted $query): void {
+            if ($query->time > 100) {
+                \Sentry\captureMessage(
+                    message: 'Database query exceeded 100ms',
+                    hint: EventHint::fromArray(['extra' => ['query' => $query->sql]])
+                );
+            }
+        });
+
+        // @phan-suppress-next-line PhanTypeArraySuspicious
+        $this->app[Kernel::class]->whenRequestLifecycleIsLongerThan(
+            100,
+            static function (Carbon $startedAt, Request $request, Response $response): void {
+                \Sentry\captureMessage('Request exceeded 100ms');
+            }
+        );
+
         Attendance::observe(AttendanceObserver::class);
         DocuSignEnvelope::observe(DocuSignEnvelopeObserver::class);
         DuesPackage::observe(DuesPackageObserver::class);
@@ -69,7 +111,7 @@ class AppServiceProvider extends ServiceProvider
         TravelAssignment::observe(TravelAssignmentObserver::class);
         User::observe(UserObserver::class);
 
-        Relation::morphMap([
+        Relation::enforceMorphMap([
             'dues-transaction' => DuesTransaction::class,
             'event' => Event::class,
             'signature' => Signature::class,
