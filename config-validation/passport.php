30cf53cf2c152b6cf76abcbee1b537cb24753d22
Kristaps Berzinch <kristapsberzinch@gmail.com>
2022-12-11 16:58:48 -0500

    Fix config validation for google-play-review env

--- config-validation/passport.php
+++ config-validation/passport.php
@@ -9,27 +9,31 @@ return [
         ->rules([
             'required',
             'string',
-        ]),
+        ])
+        ->environments(['test', Rule::ENV_PRODUCTION]),
 
     Rule::make('public_key')
         ->rules([
             'required',
             'string',
-        ]),
+        ])
+        ->environments(['test', Rule::ENV_PRODUCTION]),
 
     Rule::make('personal_access_client.id')
         ->rules([
             'required',
             'string',
             'alpha_dash',
-        ]),
+        ])
+        ->environments(['test', Rule::ENV_PRODUCTION]),
 
     Rule::make('personal_access_client.secret')
         ->rules([
             'required',
             'string',
             'alpha_num',
-        ]),
+        ])
+        ->environments(['test', Rule::ENV_PRODUCTION]),
 
     Rule::make('cookie_name')
         ->rules([
