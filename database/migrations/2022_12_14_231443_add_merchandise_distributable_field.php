388bd14bbcd8156a9c543d45f2161304c2ab54e1
Evan Strat <evan10s@users.noreply.github.com>
2022-12-17 15:32:06 -0500

    Import Str class

--- database/migrations/2022_12_14_231443_add_merchandise_distributable_field.php
+++ database/migrations/2022_12_14_231443_add_merchandise_distributable_field.php
@@ -6,6 +6,7 @@ use App\Models\Merchandise;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
