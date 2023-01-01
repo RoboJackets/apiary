f3388b2978f9c604ea3b2be192b1817c55471f30
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-01-01 13:47:05 -0500

    Allow lazy-loading in console commands

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -68,7 +68,8 @@ class AppServiceProvider extends ServiceProvider
 
         Model::shouldBeStrict();
 
-        if ($this->app->isProduction()) {
+        // Lazy-loading needs to be allowed for console commands due to https://github.com/laravel/scout/issues/462
+        if ($this->app->isProduction() && !$this->app->runningInConsole()) {
             Model::handleLazyLoadingViolationUsing(static function (Model $model, string $relation): void {
                 \Sentry\captureMessage('Attemped to lazy-load '.$relation.' on '.$model::class);
             });
