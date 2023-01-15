ace1f5c698dd3941190fd02d2ea595268e23fe8a
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-01-15 15:40:48 -0500

    Silence false positive PHPCS finding

--- database/migrations/2023_01_15_141511_remove_autodesk_and_middle_name_from_users.php
+++ database/migrations/2023_01_15_141511_remove_autodesk_and_middle_name_from_users.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+// phpcs:disable SlevomatCodingStandard.ControlStructures.RequireSingleLineCondition.RequiredSingleLineCondition
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
