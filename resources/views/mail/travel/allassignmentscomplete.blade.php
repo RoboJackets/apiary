b6487c3822390218f1c55a41dbf5f734157431fe
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-02-26 13:06:20 -0500

    Tweak whitespace some more

--- resources/views/mail/travel/allassignmentscomplete.blade.php
+++ resources/views/mail/travel/allassignmentscomplete.blade.php
@@ -10,20 +10,17 @@ You can download all submitted forms from {{ config('app.name') }} at {{ route('
 Bon voyage!
 {{-- all assignments documented, not paid--}}
 @elseif($travel->assignments()->unpaid()->exists() && $travel->assignments()->needDocuSign()->doesntExist())
-All travelers have submitted travel forms for {{ $travel->name }}.
-@if($travel->assignments()->unpaid()->count() === 1)
+All travelers have submitted travel forms for {{ $travel->name }}. @if($travel->assignments()->unpaid()->count() === 1)
 1 traveler still needs to pay the travel fee.
-@else
-{{ $travel->assignments()->unpaid()->count() }} travelers still need to pay the travel fee.
+@else{{ $travel->assignments()->unpaid()->count() }} travelers still need to pay the travel fee.
 @endif
+
 You can download all submitted forms from {{ config('app.name') }} at {{ route('nova.pages.detail', ['resource' => \App\Nova\Travel::uriKey(), 'resourceId' => $travel->id]) }}, under the action menu (three dots in top right).
 {{-- all assignments paid, not documented --}}
 @elseif($travel->assignments()->unpaid()->doesntExist() && $travel->assignments()->needDocuSign()->exists())
-All travelers have paid the travel fee for {{ $travel->name }}.
-@if($travel->assignments()->needDocuSign()->count() === 1)
+All travelers have paid the travel fee for {{ $travel->name }}. @if($travel->assignments()->needDocuSign()->count() === 1)
 1 traveler still needs to submit forms.
-@else
-{{ $travel->assignments()->needDocuSign()->count() }} travelers still need to submit forms.
+@else{{ $travel->assignments()->needDocuSign()->count() }} travelers still need to submit forms.
 @endif
 
 @endif
