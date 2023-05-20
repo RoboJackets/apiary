dd336ef0c2954a84c8cb1efb42a282ffa50408a5
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-05-20 12:58:14 -0400

    Fix deadlock for DocuSign acknowledgement email

--- app/Observers/DocuSignEnvelopeObserver.php
+++ app/Observers/DocuSignEnvelopeObserver.php
@@ -72,22 +72,25 @@ class DocuSignEnvelopeObserver
         SendReminders::dispatch($envelope->signedBy);
         PushToJedi::dispatch($envelope->signedBy, DocuSignEnvelope::class, $envelope->id, 'saved');
 
-        Cache::lock(name: 'send_acknowledgement_'.$envelope->id, seconds: 120)->block(
-            seconds: 60,
-            callback: static function () use ($envelope): void {
-                if (
-                    $envelope->complete &&
-                    $envelope->envelope_id !== null &&
-                    $envelope->signable_type === Signature::getMorphClassStatic() &&
-                    ! $envelope->acknowledgement_sent
-                ) {
-                    $envelope->acknowledgement_sent = true;
-                    $envelope->save();
+        if (! $envelope->acknowledgement_sent) {
+            Cache::lock(name: 'send_acknowledgement_'.$envelope->id, seconds: 120)->block(
+                seconds: 60,
+                callback: static function () use ($envelope): void {
+                    $envelope->refresh();
+                    if (
+                        $envelope->complete &&
+                        $envelope->envelope_id !== null &&
+                        $envelope->signable_type === Signature::getMorphClassStatic() &&
+                        ! $envelope->acknowledgement_sent // @phpstan-ignore-line
+                    ) {
+                        $envelope->acknowledgement_sent = true;
+                        $envelope->save();
 
-                    $envelope->signedBy->notify(new MembershipAgreementDocuSignEnvelopeReceived($envelope));
+                        $envelope->signedBy->notify(new MembershipAgreementDocuSignEnvelopeReceived($envelope));
+                    }
                 }
-            }
-        );
+            );
+        }
 
         if ($envelope->complete && $envelope->signable_type === Signature::getMorphClassStatic()) {
             $envelope->signable->complete = true;
