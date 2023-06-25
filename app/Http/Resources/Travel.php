c43292edbb123f65214a0b772c15f356596d006c
Evan Strat <evan10s@users.noreply.github.com>
2023-06-25 18:32:45 -0400

    Pint cleanup

--- app/Http/Resources/Travel.php
+++ app/Http/Resources/Travel.php
@@ -12,9 +12,6 @@ class Travel extends JsonResource
 {
     /**
      * Transform the resource into an array.
-     *
-     * @param  Request  $request
-     * @return array
      */
     public function toArray(Request $request): array
     {
