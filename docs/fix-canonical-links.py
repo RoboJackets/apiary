d78ace42f5e81cce038740dd630331e611f17bdd
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-06-25 21:41:28 -0400

    Fix broken doc pages

--- docs/fix-canonical-links.py
+++ docs/fix-canonical-links.py
@@ -6,4 +6,4 @@
 from re import sub
 from sys import stdin
 
-print(sub(r"<link rel=\"canonical\" href=\"(?P<url>.+)\.html\" \/>", "<link rel=\"canonical\" href=\"\\g<url>/\" \\/>", stdin.read().strip()))
+print(sub(r"<link rel=\"canonical\" href=\"(?P<url>.+)\.html\" \/>", "<link rel=\"canonical\" href=\"\\g<url>/\" />", stdin.read().strip()))
