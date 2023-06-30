89f5b50631facbf6d0804634ef7805b355cb3919
Kristaps Berzinch <kristapsberzinch@gmail.com>
2023-06-30 13:17:52 +0300

    Handle index.html canonical links correctly

--- docs/fix-canonical-links.py
+++ docs/fix-canonical-links.py
@@ -3,7 +3,16 @@
 # workaround for a sphinx issue
 # https://github.com/sphinx-doc/sphinx/issues/9730
 
-from re import sub
+from re import search
 from sys import stdin
 
-print(sub(r"<link rel=\"canonical\" href=\"(?P<url>.+)\.html\" \/>", "<link rel=\"canonical\" href=\"\\g<url>/\" />", stdin.read().strip()))
+html = stdin.read().strip()
+
+url = search(r"<link rel=\"canonical\" href=\"(?P<url>.+)\" \/>", html).group("url")
+
+if url.endswith("/index.html"):
+    newurl = url[:-10]
+else:
+    newurl = url[:-5] + "/"
+
+print(html.replace(url, newurl))
