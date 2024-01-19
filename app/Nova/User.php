cb5b6ce60e5e8a55b9d929beea9959499522c84d
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2024-01-18 21:37:58 -0500

    Update app/Nova/User.php
    Co-authored-by: Kristaps Berzinch <kristapsberzinch@gmail.com>

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -171,10 +171,7 @@ class User extends Resource
                 ->onlyOnDetail()
                 ->hideFromDetail(static fn (NovaRequest $r, AppModelsUser $u): bool => $u->is_service_account),
 
-            Text::make(
-                'Graduation Semester',
-                static fn (AppModelsUser $u): string => $u->getHumanReadableSemesterAttribute()
-            )
+            Text::make('Graduation Semester', 'human_readable_graduation_semester')
                 ->onlyOnDetail()
                 ->hideFromDetail(static fn (NovaRequest $r, AppModelsUser $u): bool => $u->is_service_account),
 
