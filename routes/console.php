4eda0c60ccb7beb776da07a033a0b7a602db0aff
Shift <shift@laravelshift.com>
2024-04-28 20:12:52 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Jobs\DailyDuesSummary;
+use App\Jobs\NoAttendanceJediPush;
+use App\Jobs\PruneAccessFromAccessInactiveUsers;
+use App\Jobs\SendExpiringPersonalAccessTokenNotifications;
+use App\Jobs\SendRemindersForExpiringAccessOverrides;
+use App\Jobs\SendRemindersForExpiringDuesPackages;
+use App\Jobs\WeeklyAttendanceSlack;
+use Illuminate\Support\Facades\Schedule;
+use UKFast\HealthCheck\Commands\CacheSchedulerRunning;
+
 declare(strict_types=1);
 
 /*
@@ -12,3 +22,19 @@ declare(strict_types=1);
 | simple approach to interacting with each command's IO methods.
 |
 */
+
+
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
+Schedule::command(CacheSchedulerRunning::class)->everyMinute();
+Schedule::command('passport:purge')->twiceDaily();
+
+Schedule::job(new WeeklyAttendanceSlack())->weekly()->sundays()->at('11:00');
+Schedule::job(new SendExpiringPersonalAccessTokenNotifications())->weekly()->mondays()->at('08:00');
+Schedule::job(new DailyDuesSummary())->daily()->at('00:00');
+Schedule::job(new NoAttendanceJediPush())->daily()->at('10:00');
+Schedule::job(new SendRemindersForExpiringAccessOverrides())->daily()->at('04:00');
+Schedule::job(new SendRemindersForExpiringDuesPackages())->daily()->at('08:00');
+
+if (config('features.prune-access') === true) {
+    Schedule::job(new PruneAccessFromAccessInactiveUsers())->daily()->at('04:00');
+}
