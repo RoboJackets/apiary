003c79e6d3923033bcfa81e147600505556f6be8
Shift <shift@laravelshift.com>
2024-04-28 20:12:47 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,39 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login.cas',['next'=>url()->full()]));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->validateCsrfTokens(except: [
+            'apiv3/*',
+        ]);
+
+        $middleware->web([
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+            \App\Http\Middleware\Sentry::class,
+            \Spatie\Csp\AddCspHeaders::class,
+            \HTMLMin\HTMLMin\Http\Middleware\MinifyMiddleware::class,
+        ]);
+
+        $middleware->throttleApi('180,1');
+        $middleware->api(\App\Http\Middleware\Sentry::class);
+
+        $middleware->alias([
+            'auth.cas.check' => \App\Http\Middleware\CasCheck::class,
+            'auth.cas.force' => \App\Http\Middleware\CasAuthenticate::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        ]);
+
+        $middleware->priority([
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\Authenticate::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Auth\Middleware\Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
