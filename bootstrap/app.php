8618cf8f68ecbd404505c25454c5a85ac49b099c
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-06-23 15:50:01 -0400

    Remove MinifyMiddleware since it's doing something strange with Horizon

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,6 @@ return Application::configure(basePath: dirname(__DIR__))
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
             \App\Http\Middleware\Sentry::class,
             \Spatie\Csp\AddCspHeaders::class,
-            \HTMLMin\HTMLMin\Http\Middleware\MinifyMiddleware::class,
         ]);
 
         $middleware->throttleApi('180,1');
