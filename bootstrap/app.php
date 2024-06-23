bf25e61758bc23657d749e65933ed3de52cc33de
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-06-23 13:58:56 -0400

    Disable minification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
             \App\Http\Middleware\Sentry::class,
             \Spatie\Csp\AddCspHeaders::class,
-            \HTMLMin\HTMLMin\Http\Middleware\MinifyMiddleware::class,
+            // \HTMLMin\HTMLMin\Http\Middleware\MinifyMiddleware::class,
         ]);
 
         $middleware->throttleApi('180,1');
