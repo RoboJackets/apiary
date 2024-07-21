521096a5fceeb88f52b0069bf26018d5f77e42b0
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-07-21 15:56:17 -0400

    style

--- config/responsecache.php
+++ config/responsecache.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     /*
      * Determine if the response cache middleware should be enabled.
@@ -28,7 +30,7 @@ return [
      * When using the default CacheRequestFilter this setting controls the
      * default number of seconds responses must be cached.
      */
-    'cache_lifetime_in_seconds' => (int) env('RESPONSE_CACHE_LIFETIME', 60 * 60 * 24 * 7),
+    'cache_lifetime_in_seconds' => intval(env('RESPONSE_CACHE_LIFETIME', 60 * 60 * 24 * 7)),
 
     /*
      * This setting determines if a http header named with the cache time
