4562e242cc27b5cb8227e0ff518f24dacb421a22
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-09-09 21:20:50 -0400

    Allow sorting events by start or end time

--- app/Nova/Event.php
+++ app/Nova/Event.php
@@ -93,10 +93,12 @@ class Event extends Resource
                 ->withoutTrashed(),
 
             DateTime::make('Start Time')
+                ->sortable()
                 ->required()
                 ->rules('required', 'date', 'before:end_time'),
 
             DateTime::make('End Time')
+                ->sortable()
                 ->required()
                 ->rules('required', 'date', 'after:start_time'),
 
