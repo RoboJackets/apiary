a4797e52974f2cb8ce1a152cf89d654bd37e906e
jvogt23 <jamesvogt70@gmail.com>
2024-09-22 18:45:45 -0400

    Added migration - add class standings as of 2024-09-22

---
+++ database/migrations/2024_09_22_161700_add_class_standings.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use App\Models\ClassStanding;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        ClassStanding::firstOrCreate(
+            ['name' => 'senior'],
+            ['rank_order' => 4]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'junior'],
+            ['rank_order' => 3]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'masters'],
+            ['rank_order' => 5]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'sophomore'],
+            ['rank_order' => 2]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'freshman'],
+            ['rank_order' => 1]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'doctorate'],
+            ['rank_order' => 6]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'special'],
+            ['rank_order' => 0]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'jephs'],
+            ['rank_order' => -1]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'pe'],
+            ['rank_order' => 7]
+        );
+    }
+
+    public function down(): void
+    {
+        Schema::table('class_standings', function($table)
+        {
+            $table->truncate();
+        });
+    }
+};
