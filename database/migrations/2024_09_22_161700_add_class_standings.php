fdf748a6fa02d41621f46f28323ae00cfbee83c7
jvogt23 <jamesvogt70@gmail.com>
2024-09-22 21:12:20 -0400

    resolve requested changes

--- database/migrations/2024_09_22_161700_add_class_standings.php
+++ database/migrations/2024_09_22_161700_add_class_standings.php
@@ -11,36 +11,36 @@ return new class extends Migration
     public function up(): void
     {
         ClassStanding::firstOrCreate(
-            ['name' => 'senior'],
-            ['rank_order' => 4]
-        );
-        ClassStanding::firstOrCreate(
-            ['name' => 'junior'],
-            ['rank_order' => 3]
-        );
-        ClassStanding::firstOrCreate(
-            ['name' => 'masters'],
-            ['rank_order' => 5]
-        );
-        ClassStanding::firstOrCreate(
-            ['name' => 'sophomore'],
-            ['rank_order' => 2]
-        );
-        ClassStanding::firstOrCreate(
-            ['name' => 'freshman'],
-            ['rank_order' => 1]
-        );
-        ClassStanding::firstOrCreate(
-            ['name' => 'doctorate'],
-            ['rank_order' => 6]
+            ['name' => 'jephs'],
+            ['rank_order' => -1]
         );
         ClassStanding::firstOrCreate(
             ['name' => 'special'],
             ['rank_order' => 0]
         );
         ClassStanding::firstOrCreate(
-            ['name' => 'jephs'],
-            ['rank_order' => -1]
+            ['name' => 'freshman'],
+            ['rank_order' => 1]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'sophomore'],
+            ['rank_order' => 2]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'junior'],
+            ['rank_order' => 3]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'senior'],
+            ['rank_order' => 4]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'masters'],
+            ['rank_order' => 5]
+        );
+        ClassStanding::firstOrCreate(
+            ['name' => 'doctorate'],
+            ['rank_order' => 6]
         );
         ClassStanding::firstOrCreate(
             ['name' => 'pe'],
