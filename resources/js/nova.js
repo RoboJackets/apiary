759bead733a3a35e36e90bedadb225e4b48c7289
Kristaps Berzinch <kristapsberzinch@gmail.com>
2024-10-20 12:18:22 -0400

    Add triggerLabel to Nova feedback widget

--- resources/js/nova.js
+++ resources/js/nova.js
@@ -42,6 +42,7 @@ Nova.booting((Vue, router) => {
                     },
                     buttonLabel: 'Feedback',
                     submitButtonLabel: 'Send Feedback',
+                    triggerLabel: 'Feedback',
                     formTitle: 'Feedback',
                     messagePlaceholder: 'Describe what you were trying to do, what you expected, and what actually happened.',
                     successMessageText: 'Thank you for your feedback!',
