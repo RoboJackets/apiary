63b4e8b25466f46b2a86138def02cbf298fbaf12
jvogt23 <jamesvogt70@gmail.com>
2024-11-04 21:01:01 -0500

    Initial changes made; needs testing

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,7 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-    window.Popper = require('popper.js').default;
+    window.Popper = require('@popperjs/core').default;
     require('bootstrap');
 } catch (e) {
 }
