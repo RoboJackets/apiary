339010d8ca66292eb5ec3ed7a7ee25e0ff4a8cc2
jvogt23 <jamesvogt70@gmail.com>
2024-11-17 21:37:07 -0500

    Removed JQuery per Bootstrap 5 dependency changes

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,6 @@ window._ = require('lodash');
  */
 
 try {
-    window.$ = window.jQuery = require('jquery');
     window.Popper = require('@popperjs/core').default;
     require('bootstrap');
 } catch (e) {
