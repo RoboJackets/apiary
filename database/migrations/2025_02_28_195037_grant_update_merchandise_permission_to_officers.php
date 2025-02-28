9b4ddda363a3ef0993cc977675e80aff2da403f2
Kristaps Berzinch <kristapsberzinch@gmail.com>
2025-02-28 15:00:02 -0500

    Remove unused uses

--- database/migrations/2025_02_28_195037_grant_update_merchandise_permission_to_officers.php
+++ database/migrations/2025_02_28_195037_grant_update_merchandise_permission_to_officers.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
