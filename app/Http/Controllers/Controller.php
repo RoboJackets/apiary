68bbeea071696db557f2403f1403c71938d1f7fe
Shift <shift@laravelshift.com>
2025-03-07 18:36:59 +0000

    Remove base controller inheritance

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use Illuminate\Routing\Controller as BaseController;
 
-abstract class Controller extends BaseController
+abstract class Controller
 {
 }
