09ffece2be36443a61e4ab84c31e2e9da3549b23
Kristaps Berzinch <kristapsberzinch@gmail.com>
2025-03-07 20:45:06 -0500

    Filter out empty strings from sandbox users list

--- config/features.php
+++ config/features.php
@@ -6,7 +6,10 @@ return [
     'resumes' => env('FEATURE_ENABLE_RESUMES', false),
     'card-present-payments' => env('FEATURE_ENABLE_CARD_PRESENT_PAYMENTS', false),
     'sandbox-mode' => env('FEATURE_SANDBOX_MODE', false),
-    'sandbox-users' => explode(',', env('SANDBOX_USERS', '')),
+    'sandbox-users' => array_filter(
+        explode(',', env('SANDBOX_USERS', '')),
+        static fn (string $value): bool => $value !== ''
+    ),
     'whitepages' => env('FEATURE_ENABLE_WHITEPAGES', true),
     'prune-access' => env('FEATURE_PRUNE_ACCESS', ! env('APP_DEBUG', false)),
 ];
