9172c89313faebcd9cb7ce694350ad7dab741bde
Kristaps Berzinch <kristapsberzinch@gmail.com>
2025-07-13 13:15:34 -0500

    Fix poststart script

--- .nomad/scripts/poststart.sh
+++ .nomad/scripts/poststart.sh
@@ -1,7 +1,2 @@
-php artisan scout:sync-index-settings --no-interaction --verbose || true
-if ! php artisan scout:indexes-ready --no-interaction --verbose
-then
-    php artisan scout:import-all --no-interaction --verbose
-else
-    php artisan scout:import \\App\\Models\\Airport --no-interaction --verbose
-fi
+php artisan scout:sync-index-settings --no-interaction --verbose
+php artisan scout:import-all --no-interaction --verbose
