af4c1cd44b240b80d4e3c630280b74bdfcedcc39
Kristaps Berzinch <kristapsberzinch@gmail.com>
2025-09-25 17:25:05 -0400

    Add additional context for access expiration

--- resources/views/mail/dues/packageexpirationreminder.blade.php
+++ resources/views/mail/dues/packageexpirationreminder.blade.php
@@ -4,7 +4,7 @@ The following dues {{ count($packages) === 1 ? 'deadline is' : 'deadlines are' }
 - {{ $package->name }} expires on {{ $package->access_end->format('l, F j') }}
 @endforeach
 
-{{ $userCount }} {{ \Illuminate\Support\Str::plural('member', $userCount) }} will lose access to RoboJackets systems.
+{{ $userCount }} {{ \Illuminate\Support\Str::plural('member', $userCount) }} will lose access to RoboJackets systems, unless they renew their {{ \Illuminate\Support\Str::plural('membership', $userCount) }} before the expiration {{ \Illuminate\Support\Str::plural('date', count($packages)) }}.
 
 You may update the "Access End Date" for {{ count($packages) === 1 ? 'this package' : 'these packages' }} at {{ count($packages) === 1 ? route('nova.pages.detail', ['resource' => \App\Nova\DuesPackage::uriKey(), 'resourceId' => $packages->sole()->id]) : route('nova.pages.index', ['resource' => \App\Nova\DuesPackage::uriKey()]) }}. If the {{ count($packages) === 1 ? 'date is' : 'dates are' }} correct, you may ignore this email.
 
