dfd85a33813a94ba994ee74b107fd960f5691ab6
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2025-10-11 19:48:30 -0400

    Added SponsorUser model

---
+++ app/Models/SponsorUser.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * This model represents a sponsor entity in the application.
+ *
+ * @property int $id
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ */
+class Sponsor extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'email',
+    ];
+
+    /**
+     * Get the sponsor company that this SponsorUser belongs to
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Sponsor::class, 'sponsor_id');
+    }
+}
