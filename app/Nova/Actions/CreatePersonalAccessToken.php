737bf6ca35be48d9ecb4fbfb544c642e8ff35cae
Kristaps Berzinch <kristapsberzinch@gmail.com>
2025-10-17 18:46:24 -0400

    Fix Phan suppressions

--- app/Nova/Actions/CreatePersonalAccessToken.php
+++ app/Nova/Actions/CreatePersonalAccessToken.php
@@ -42,8 +42,6 @@ class CreatePersonalAccessToken extends Action
      * Perform the action on the given models.
      *
      * @param  \Illuminate\Support\Collection<int,\App\Models\User>  $models
-     *
-     * @phan-suppress PhanTypeMismatchArgument
      */
     public function handle(ActionFields $fields, Collection $models)
     {
