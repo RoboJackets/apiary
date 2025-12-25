0bd1aebeee04b1188f9231950b07ae9ad2c3bd0b
Kristaps Berzinch <kristapsberzinch@gmail.com>
2025-12-25 10:50:26 -0600

    Add action event logs to OAuth clients

--- app/Models/OAuth2Client.php
+++ app/Models/OAuth2Client.php
@@ -9,6 +9,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Auth\Access\Authorizable as AuthorizableTrait;
+use Laravel\Nova\Actions\Actionable;
 use Laravel\Passport\Client;
 use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
@@ -53,6 +54,7 @@ use Spatie\Permission\Traits\HasRoles;
  */
 class OAuth2Client extends Client implements AuthorizableContract
 {
+    use Actionable;
     use AuthorizableTrait;
     use HasPermissions;
     use HasRoles;
