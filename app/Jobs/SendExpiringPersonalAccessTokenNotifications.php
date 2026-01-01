8e94e237673ca0d7ed721d536cc1efc133a81ef6
Kristaps Berzinch <kristapsberzinch@gmail.com>
2025-12-31 19:00:56 -0500

    Remove unused `use`

--- app/Jobs/SendExpiringPersonalAccessTokenNotifications.php
+++ app/Jobs/SendExpiringPersonalAccessTokenNotifications.php
@@ -8,7 +8,6 @@ use App\Notifications\ExpiringPersonalAccessTokenNotification;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
