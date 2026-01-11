4405c2ae12a5543714c76205709d051c37745365
jvogt23 <jamesvogt70@gmail.com>
2026-01-10 21:27:56 -0600

    Linter appeasement

--- app/Notifications/SponsorOneTimePasswordNotification.php
+++ app/Notifications/SponsorOneTimePasswordNotification.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -34,7 +35,7 @@ class SponsorOneTimePasswordNotification extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
-        return (new MailMessage)
+        return (new MailMessage())
             ->subject('RoboJackets One-Time Password')
             ->view('mail.sponsor-otp', [
                 'otp' => $notifiable->currentAccessToken,
