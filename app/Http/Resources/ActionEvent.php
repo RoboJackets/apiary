5cf15c838302feb98da429f831d47ccfdd008e07
Kristaps Berzinch <kristapsberzinch@gmail.com>
2026-02-08 12:19:24 -0500

    Add `Override` annotation for `ActionEvent` resource

--- app/Http/Resources/ActionEvent.php
+++ app/Http/Resources/ActionEvent.php
@@ -14,6 +14,7 @@ class ActionEvent extends JsonResource
      *
      * @return array<string, mixed>
      */
+    #[\Override]
     public function toArray(Request $request): array
     {
         return [
