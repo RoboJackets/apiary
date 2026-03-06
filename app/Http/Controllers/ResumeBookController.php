5461795cecc622de724123948790bae18d0effb6
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2026-03-05 20:56:19 -0600

    add return statement

--- app/Http/Controllers/ResumeBookController.php
+++ app/Http/Controllers/ResumeBookController.php
@@ -96,5 +96,6 @@ class ResumeBookController
                 'graduation_semester' => $this->formatGradSemester($user->graduation_semester),
             ]))
             ->toArray();
+        return $users;
     }
 }
