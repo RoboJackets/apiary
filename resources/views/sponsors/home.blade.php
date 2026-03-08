bf88501e0e2e6d20590054ab865b0bafef46e33f
jvogt23 <jamesvogt70@gmail.com>
2026-03-07 21:44:08 -0600

    Add comments and clean up code

--- resources/views/sponsors/home.blade.php
+++ resources/views/sponsors/home.blade.php
@@ -5,7 +5,5 @@ Resume Book | {{ config('app.name') }}
 @endsection
 
 @section('content')
-
-<!-- <resume-book-index :users='@json($users)'></resume-book-index> -->
-<resume-book-index-test></resume-book-index-test>
+<resume-book-index></resume-book-index>
 @endsection
