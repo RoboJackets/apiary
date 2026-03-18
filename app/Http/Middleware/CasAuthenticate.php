5f130e681c2cd823f0847d771a430d8204a55fd3
jvogt23 <jamesvogt70@gmail.com>
2026-03-17 21:08:21 -0500

    Located an issue with session collisions

--- app/Http/Middleware/CasAuthenticate.php
+++ app/Http/Middleware/CasAuthenticate.php
@@ -37,9 +37,6 @@ class CasAuthenticate
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // if (Auth::guard('sponsor')->check()) {
-        //     Auth::guard('sponsor')->logout();
-        // }
         // Check to ensure the request isn't already authenticated through the API guard
         if (! Auth::guard('api')->check()) {
             // Run the user update only if they don't have an active session
@@ -84,8 +81,16 @@ class CasAuthenticate
             if ($request->ajax() || $request->wantsJson()) {
                 return response('Unauthorized', 401);
             }
+
+            $sponsor_logged_in = false;
+            if (Auth::guard('sponsor')->check()) {
+                $sponsor_logged_in = true;
+                Auth::guard('sponsor')->logout();
+            }
+
             Cas::authenticate();
-            if (Cas::isAuthenticated() && $request->user() instanceof \App\Models\SponsorUser) {
+
+            if (Cas::isAuthenticated() && $sponsor_logged_in) {
                 $user = CasUser::createOrUpdate();
                 Auth::login($user);
             }
