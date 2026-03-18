9a94ab8536bbc650b62ca6a7ff1fb7fb494f5fe5
jvogt23 <jamesvogt70@gmail.com>
2026-03-17 21:25:13 -0500

    Located an issue with session collisions

--- app/Http/Middleware/CasAuthenticate.php
+++ app/Http/Middleware/CasAuthenticate.php
@@ -37,6 +37,12 @@ class CasAuthenticate
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (Auth::guard('sponsor')->check()) {
+            Auth::guard('sponsor')->logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+            Auth::logout();
+        }
         // Check to ensure the request isn't already authenticated through the API guard
         if (! Auth::guard('api')->check()) {
             // Run the user update only if they don't have an active session
@@ -82,18 +88,7 @@ class CasAuthenticate
                 return response('Unauthorized', 401);
             }
 
-            $sponsor_logged_in = false;
-            if (Auth::guard('sponsor')->check()) {
-                $sponsor_logged_in = true;
-                Auth::guard('sponsor')->logout();
-            }
-
             Cas::authenticate();
-
-            if (Cas::isAuthenticated() && $sponsor_logged_in) {
-                $user = CasUser::createOrUpdate();
-                Auth::login($user);
-            }
         }
 
         // User is authenticated through the API guard (I guess? Moving this into an else() broke sessions)
