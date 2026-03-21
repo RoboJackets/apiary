10899f60e5d2fc87413980d2c72742e18d9561c6
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2026-03-21 11:12:48 -0500

    Update app/Http/Controllers/ResumeBookController.php
    Co-authored-by: Kristaps Berzinch <kristapsberzinch@gmail.com>

--- app/Http/Controllers/ResumeBookController.php
+++ app/Http/Controllers/ResumeBookController.php
@@ -108,7 +108,7 @@ class ResumeBookController
         $users = User::active()->whereIn('uid', $usernames);
 
         if (! ($majors === [])) {
-            $users = $users->whereHas('majors', static fn ($query) => $query->whereIn('majors.display_name', $majors));
+            $users = $users->whereHas('majors', static fn (Builder $query): Builder => $query->whereIn('majors.display_name', $majors));
         }
 
         if (! ($graduation_semesters === [])) {
