1ae357b9ce579f61961413c463b6aef0255941b1
jvogt23 <jamesvogt70@gmail.com>
2026-03-27 22:55:07 -0500

    Remove unused import

--- app/Models/SponsorUser.php
+++ app/Models/SponsorUser.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use BadMethodCallException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
