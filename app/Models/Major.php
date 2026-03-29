5ac2614b743924f613dc8e4660160a86cb8a0b5b
jvogt23 <jamesvogt70@gmail.com>
2026-03-28 21:40:09 -0500

    linter appeasement

--- app/Models/Major.php
+++ app/Models/Major.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
