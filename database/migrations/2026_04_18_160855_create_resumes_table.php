a9596bf69d5f6ae54bcb956f0c88244b36b1d753
jvogt23 <jamesvogt70@gmail.com>
2026-04-18 15:21:18 -0500

    Linter

--- database/migrations/2026_04_18_160855_create_resumes_table.php
+++ database/migrations/2026_04_18_160855_create_resumes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('resumes', function (Blueprint $table) {
+        Schema::create('resumes', static function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
             $table->string('file_name')->nullable();
