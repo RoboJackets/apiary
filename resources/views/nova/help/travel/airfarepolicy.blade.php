c475d4c56e341c7471e29910dd57d4d780fd9cf9
Kristaps Berzinch <kristapsberzinch@gmail.com>
2026-05-19 16:34:02 -0400

    Clarify airfare policy help text for normal users without permissions

--- resources/views/nova/help/travel/airfarepolicy.blade.php
+++ resources/views/nova/help/travel/airfarepolicy.blade.php
@@ -5,7 +5,7 @@
 @elseif(request()->user()->hasRole('officer'))
     You do not have permission to change the airfare policy. If you need access, ask in <a href="https://robojackets.slack.com/app_redirect?channel=it-helpdesk" target="_blank">#it-helpdesk</a>.
 @elseif(str_contains(request()->path(), 'update-fields'))
-    The airfare policy for this trip can be changed by an officer, if needed.
+    You do not have permission to change the airfare policy. The airfare policy for this trip can be changed by an officer, if needed.
 @else
-    The airfare policy for this trip can be changed by an officer after creating the trip, if needed.
+    You do not have permission to change the airfare policy. The airfare policy for this trip can be changed by an officer after creating the trip, if needed.
 @endif
