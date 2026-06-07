fd88681d95521bee64afabe561cae0d612cedd6b
Kristaps Berzinch <kristapsberzinch@gmail.com>
2026-06-07 11:57:09 -0400

    Fix missing `use`

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\Traits\GetMorphClassStatic;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
