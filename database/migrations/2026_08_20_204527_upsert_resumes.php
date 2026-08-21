adfb362afabd330b26ae34e23e165c84e4ca8f0c
jvogt23 <jamesvogt70@gmail.com>
2026-08-21 16:11:35 -0400

    Remove unused imports, declare strict types

--- database/migrations/2026_08_20_204527_upsert_resumes.php
+++ database/migrations/2026_08_20_204527_upsert_resumes.php
@@ -1,11 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Models\Resume;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 
 return new class extends Migration
