7262565000ad16b5021887451e1e801601158da1
Evan Strat <5790137+evan10s@users.noreply.github.com>
2026-08-25 23:22:08 -0400

    Create 2026_08_25_112001_add_debug_mrd5_permission.php

---
+++ database/migrations/2026_08_25_112001_add_debug_mrd5_permission.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Spatie\Permission\Models\Permission;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        app()['cache']->forget('spatie.permission.cache');
+
+        $debug_mrd5 = Permission::firstOrCreate(['name' => 'debug-mrd5']);
+
+        $r_admin = Role::firstOrCreate(['name' => 'admin']);
+        $r_admin->givePermissionTo($debug_mrd5);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        app()['cache']->forget('spatie.permission.cache');
+        Permission::where('name', 'debug-mrd5')->delete();
+    }
+};
