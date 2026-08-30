bae69bc684a98447db9ab52b76b238430fa02880
jvogt23 <jamesvogt70@gmail.com>
2026-08-30 15:34:33 -0400

    Change migration to avoid updating timestamps

--- database/migrations/2026_08_20_204527_upsert_resumes.php
+++ database/migrations/2026_08_20_204527_upsert_resumes.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
-use App\Models\Resume;
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 
 return new class extends Migration
@@ -17,11 +18,32 @@ return new class extends Migration
         $resumes = Storage::disk('local')->files('resumes');
         $resumes = array_map(static fn ($f) => pathinfo($f, PATHINFO_FILENAME), $resumes);
 
-        $users = User::whereIn('uid', $resumes)->get();
-        Resume::upsert(
-            $users->map(static fn ($user) => ['user_id' => $user->id])->all(),
-            ['user_id']
-        );
+        Schema::table('resumes', static function (Blueprint $table) {
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('user')
+                ->onDelete('cascade');
+
+            $table->unique('user_id');
+        });
+
+        DB::table('users')->select('id', 'resume_date')
+            ->orderBy('id')
+            ->chunk(200, static function ($rows) {
+                $data = $rows->map(static function ($row) {
+                    return [
+                        'user_id' => $row->id,
+                        'updated_at' => $row->resume_date,
+                        'created_at' => now(),
+                    ];
+                })->toArray();
+
+                DB::table('resumes')->insertOrIgnore($data);
+            });
+
+        Schema::table('users', static function (Blueprint $table) {
+            $table->dropColumn('resume_date');
+        });
     }
 
     /**
@@ -31,6 +53,19 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('users', static function (Blueprint $table) {
+            $table->dateTime('resume_date')
+                ->nullable();
+        });
+
+        DB::table('resumes')->orderBy('id')->chunk(200, static function ($rows) {
+            foreach ($rows as $row) {
+                DB::table('users')
+                    ->where('id', $row->user_id)
+                    ->update(['resume_date' => $row->updated_at]);
+            }
+        });
+
         Resume::truncate();
     }
 };
